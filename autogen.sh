3d4fb5b9739a14ca1d85afa96f8c34d738650095
Or Kehati <ork@mellanox.com>
2014-11-05 12:58:23 +0200

    issue: 434108 extract lwip - remove lwip-vma patches
    Change-Id: I5865eff2d25d647712985927b6a4671cea1e0e25
    Signed-off-by: Or Kehati <ork@mellanox.com>

--- autogen.sh
+++ autogen.sh
@@ -6,12 +6,6 @@ if [ ! -e /usr/lib64/libnl.so ] && [ -e /lib64/libnl.so.1 ]; then
 	sudo ln -s /lib64/libnl.so.1  /usr/lib64/libnl.so
 fi
 
-#Extract LWIP and apply its patches if necessary
-if [ ! -e ./configure ]; then
-	echo Extract LWIP and apply its patches if necessary
-	src/lwip-vma/lwip_extract.sh $PWD/src/
-fi
-
 set -x
 test -d ./config || mkdir -p config
 aclocal -I config
