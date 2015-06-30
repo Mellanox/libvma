d49effd23ad7ff8419bf86348567c98d87ae20ed
Avner BenHanoch <avnerb@mellanox.com>
2015-06-30 12:58:28 +0300

    issue: 561831 write() call fails with broken pipe when the server comes up after several connection retries - provide script that fails on write only when running the script under VMA - the bug is because VMA implementation for poll(2) return with wrong events on fd that had error (such as async connect failed)     hence, user wrongly believe that the connection succeded and tries to send data - This is V2 of the script in which we check for write flag on the socket as the customer does (and with some cleanup)
    Change-Id: Ieee04dfb35db4bd42487a790204314a0b99a0c63

--- tests/async-echo-client/async-echo-client.py
+++ tests/async-echo-client/async-echo-client.py
@@ -21,125 +21,90 @@
 #
 import socket, select, os, time, sys, fcntl
 import datetime as dt
-NUM_SOCKETS=1
 
-if (len(sys.argv) <4):
-    print "In correct parameter : " + sys.argv[0] + " server-ip server-port msg-for-echo [bind-IP]"
-    sys.exit(-1)
+def is_connected( sock ):
+	"use poll with zero timeout for checking if socket is writeable"
+   
+	poller = select.poll()
+	poller.register(sock, select.POLLOUT)
+	pevents = poller.poll(0)
+	print "- poll returned: %s" % str(pevents)
+	if len(pevents) == 0: return False
+	fd, flag = pevents[0]
+	return flag & select.POLLOUT
 
-IP   = sys.argv[1]
-PORT = int(sys.argv[2])
-msg  = sys.argv[3]
+def main():
+	if (len(sys.argv) <4):
+	    print "Incorrect parameter : " + sys.argv[0] + " server-ip server-port msg-for-echo [bind-IP]"
+	    sys.exit(-1)
 
-BIND_IP=None
+	IP   = sys.argv[1]
+	PORT = int(sys.argv[2])
+	msg  = sys.argv[3]
 
-if (len(sys.argv) > 4):
-    BIND_IP=sys.argv[4]
+	BIND_IP=None
 
-sock=None
-sock_fd = 0
-epoll = select.epoll(NUM_SOCKETS)
+	if (len(sys.argv) > 4):
+	    BIND_IP=sys.argv[4]
 
-counter = 0
-success = False
-while not success:
-	sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+	sock=None
+	sock_fd = 0
+	epoll = select.epoll(1) # we only have 1 socket
 
-	sock_fd = sock.fileno()
-	#sock.setblocking(0)
-	flags = fcntl.fcntl(sock_fd, fcntl.F_GETFL, 0)
-	flags = flags|os.O_NONBLOCK
-	rv = fcntl.fcntl(sock_fd, fcntl.F_SETFL, flags)
-	
-#	sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 4*1024)
-#	sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 4*1024)
-	if (BIND_IP):
-		sock.bind((BIND_IP, 0)) # need local interface IP, not server IP
+	counter = 0
+	success = False
+	while not success:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock_fd = sock.fileno()
+		sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
+		flags = fcntl.fcntl(sock_fd, fcntl.F_GETFL, 0)
+		flags = flags|os.O_NONBLOCK
+		rv = fcntl.fcntl(sock_fd, fcntl.F_SETFL, flags)
 
-	print "starting async connect..."
-	n1=dt.datetime.now()
-	err = sock.connect_ex((IP, PORT))
-	if err != 115 and err != 0:
-		print "error %d"%err
-		sys.exit (1)
-	n2=dt.datetime.now()
-	print "async connect took ", (n2-n1).microseconds, "usec"
-	
-#	epoll.register(sock_fd, select.EPOLLOUT | select.EPOLLET)
-	epoll.register(sock_fd, select.EPOLLOUT )
+		if (BIND_IP): sock.bind((BIND_IP, 0))
 
-	print "calling epoll - for getting for connect result (this is expected to return immediately in case the server is up)"
-	n1=dt.datetime.now()
-	eevents = epoll.poll(1)
-	for fd, event in eevents:
-		if fd != sock_fd: 
-			print "ERROR: bad fd"
-			sys.exit(1)
-		print "event %d"%event
-
-		# Commonly used flag setes
-		READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
-		READ_WRITE = READ_ONLY | select.POLLOUT
-		
-		# Set up the poller
-		poller = select.poll()
-		poller.register(sock, READ_WRITE)
-		pevents = poller.poll(0)
-		
-		for fd, flag in pevents:
-			if fd != sock_fd: 
-				print "ERROR: bad fd"
-				sys.exit(1)
-			print "all flag are", flag
-			print "flag=", flag & select.POLLIN, flag & select.POLLOUT
-			print "flag=", flag & select.POLLERR, flag & select.POLLHUP
-			if flag & (select.POLLERR | select.POLLHUP):
-				print counter, "connection was NOT established successfully (will retry in 1 second) - Is the server up?"
-				epoll.unregister(sock_fd)
-				sock.close()
-				time.sleep(1)
-			else:
-				epoll.unregister(sock_fd)
-				success = True
-			
-		'''
-		err = sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
-		if err != 0:
+		print "starting async connect..."
+		err = sock.connect_ex((IP, PORT))
+		if err != 115 and err != 0:
 			print "error %d"%err
+			sys.exit (1)
+		
+	#	epoll.register(sock_fd, select.EPOLLOUT | select.EPOLLET)
+		epoll.register(sock_fd, select.EPOLLOUT )
+
+
+		print "calling epoll - for getting for connect result (this is expected to return immediately in case the server is up)"
+		eevents = epoll.poll(1)
+		print "- epoll returned: %s" % str(eevents)
+		if len(eevents) == 0: continue
+		fd, event = eevents[0]
+
+		if is_connected( sock ):
+			success = True
+		else:
 			print counter, "connection was NOT established successfully (will retry in 1 second) - Is the server up?"
-			epoll.unregister(sock_fd)
 			sock.close()
 			time.sleep(1)
-		else:
-			epoll.unregister(sock_fd)
-			success = True
-		'''
+			counter += 1		
 
-		counter += 1
-		break
-	pass
-	
-n2=dt.datetime.now()
-delta=(n2-n1).seconds
-print "connection established successfully within %d seconds (num sockets = %d)" % (delta, NUM_SOCKETS)
+	print " **** connection established successfully after %d failures" % counter
 
-print "sending..."
-sock.send(msg)
+	print "* sending..."
+	sock.send(msg)
 
-print "sleeping for 1 second"
-time.sleep (1)
+	print "* sleeping for 1 second"
+	time.sleep (1)
 
-print "receiving..."
-data = sock.recv(1024)
-print '   *****  Received:', data
+	print "* receiving..."
+	data = sock.recv(1024)
+	print ' **** Received:', data
 
-epoll.close()
+	print "starting disconnect..."
+	epoll.close()
+	sock.close()
+	print "Done..."
 
-print "starting disconnect..."
-n1=dt.datetime.now()
-sock.close()
-n2=dt.datetime.now()
-print "disconnect took ", (n2-n1).microseconds, "usec"
-print "Done..."
+
+if __name__ == "__main__":
+    main()
