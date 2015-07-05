04cdeea9bfe55e6351feca77a69a8a9333cd36e4
Avner BenHanoch <avnerb@mellanox.com>
2015-07-05 11:57:16 +0300

    issue: 561831 write() call fails with broken pipe when the server comes up after several connection retries  - rewrote many things in the script and completed it to do exactly the customer test that failed.  - do the above in a flexible way that is easy for modifications and checking various things.  - (on the way provide additional script for checking poll with a blocking socket)
    Algo:
     The condition check on poll() is used to skip handling the connection if it is not writable yet (ABH: and no errors yet),
     the check for success or failure of connection is done in the else part using the events obtained through epoll_wait.
    
            // Check if socket is writable.
            struct pollfd f;
            f.fd      = <some fd>;
            f.events  = POLLOUT;
            f.revents = 0;
            if (poll(&f,1,0)==0 && f.revents==0) { // timeout skip processing now
            } else {
                if (epollerr/epollhup) {
                    <connection failed>;
                } else {
                    <connection accepted>;
                }
    
     All this is souround in epoll loop.  The loop is also responsible to create the
     spcket and connect it in case previous attempt failed on error
    
    Change-Id: I2ce86098602faa3ed6e43573b71ac8e7d5b3bcc7

--- tests/async-echo-client/async-echo-client.py
+++ tests/async-echo-client/async-echo-client.py
@@ -13,39 +13,35 @@
 #
 #@author: Avner BenHanoch
 
-#@date: 20Jube2015
+#@date: 20June2015
 #
 # This script performs non blocking connect to a given TCP server
 # It can serve for checking VMA behaviour with async connect when the server
 # is up or down
 #
-import socket, select, os, time, sys, fcntl
-import datetime as dt
+import socket, select, os, time, sys, fcntl, errno
 
-def is_connected( sock ):
-	"use poll with zero timeout for checking if socket is writeable"
-   
+EPOLL_TIMEOUT=-1 # infinity
+
+def is_writeable_timeout( sock ):
+	"use poll with zero timeout for checking for timeout on writeable check (otherwise, the socket is either writeable or has errors)"
+
 	poller = select.poll()
 	poller.register(sock, select.POLLOUT)
 	pevents = poller.poll(0)
-	print "- poll returned: %s" % str(pevents)
-	if len(pevents) == 0: return False
-	fd, flag = pevents[0]
-	return flag & select.POLLOUT
+	print "- poll returned: %s (is_writeable_timeout=%s)" % (str(pevents), str(len(pevents) == 0))
+	return len(pevents) == 0
 
-def main():
-	if (len(sys.argv) <4):
-	    print "Incorrect parameter : " + sys.argv[0] + " server-ip server-port msg-for-echo [bind-IP]"
+def 	async_echo_client(argv):
+	if (len(argv) <4):
+	    print "Incorrect parameter : " + argv[0] + " server-ip server-port msg-for-echo [bind-IP]"
 	    sys.exit(-1)
 
-	IP   = sys.argv[1]
-	PORT = int(sys.argv[2])
-	msg  = sys.argv[3]
-
+	IP   = argv[1]
+	PORT = int(argv[2])
+	msg  = argv[3]
 	BIND_IP=None
-
-	if (len(sys.argv) > 4):
-	    BIND_IP=sys.argv[4]
+	if (len(argv) > 4): BIND_IP=argv[4]
 
 	sock=None
 	sock_fd = 0
@@ -54,47 +50,50 @@ def main():
 	counter = 0
 	success = False
 	while not success:
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock_fd = sock.fileno()
-		sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+		if sock == None:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			sock_fd = sock.fileno()
+			sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-		flags = fcntl.fcntl(sock_fd, fcntl.F_GETFL, 0)
-		flags = flags|os.O_NONBLOCK
-		rv = fcntl.fcntl(sock_fd, fcntl.F_SETFL, flags)
+			flags = fcntl.fcntl(sock_fd, fcntl.F_GETFL, 0)
+			flags = flags|os.O_NONBLOCK
+			rv = fcntl.fcntl(sock_fd, fcntl.F_SETFL, flags)
 
-		if (BIND_IP): sock.bind((BIND_IP, 0))
+			if (BIND_IP): sock.bind((BIND_IP, 0))
 
-		print "starting async connect..."
-		err = sock.connect_ex((IP, PORT))
-		if err != 115 and err != 0:
-			print "error %d"%err
-			sys.exit (1)
-		
-	#	epoll.register(sock_fd, select.EPOLLOUT | select.EPOLLET)
-		epoll.register(sock_fd, select.EPOLLOUT )
+			print "starting async connect..."
+			err = sock.connect_ex((IP, PORT))
+			if err != errno.EINPROGRESS and err != 0:
+				print "error %d"%err
+				sys.exit (1)
 
-		while not success:
-			print "calling epoll - for getting for connect result (this is expected to return immediately in case the server is up)"
-			eevents = epoll.poll(1)
-			print "- epoll returned: %s" % str(eevents)
-			if len(eevents) == 0: continue
-			fd, event = eevents[0]
-	
-			if is_connected( sock ):
-				success = True
-			else:
+			#epoll.register(sock_fd, select.EPOLLOUT | select.EPOLLET)
+			epoll.register(sock_fd, select.EPOLLOUT )
+
+		print "calling epoll - for getting for connect result"
+		eevents = epoll.poll(EPOLL_TIMEOUT)
+		print "- epoll returned: %s" % str(eevents)
+		if len(eevents) == 0: continue # epoll timeout
+		if is_writeable_timeout (sock):
+			pass # timeout - not writeable and no errors - call epoll again on same registered socket
+		else:
+			fd, events = eevents[0]
+			if events & (select.EPOLLERR | select.EPOLLHUP): # error on socket close it and restart from begining
 				print counter, "connection was NOT established successfully (will retry in 1 second) - Is the server up?"
-				sock.close()
-				time.sleep(1)
 				counter += 1
-				break		
+				sock.close()
+				sock = None
+				time.sleep(1)
+			else:
+				print " **** connection established successfully after %d failures" % counter
+				success = True
 
-	print " **** connection established successfully after %d failures" % counter
+
 
 	print "* sending..."
 	sock.send(msg)
 
-	print "* sleeping for 1 second"
+	print "* minor sleep before receiving..."
 	time.sleep (1)
 
 	print "* receiving..."
@@ -106,6 +105,8 @@ def main():
 	sock.close()
 	print "Done..."
 
+def main():
+	async_echo_client(sys.argv)
 
 if __name__ == "__main__":
     main()
---
+++ tests/async-echo-client/syncronized-echo-client.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+#
+#
+#@copyright:
+#        Copyright (C) Mellanox Technologies Ltd. 2001-2013.  ALL RIGHTS RESERVED.
+#        This software product is a proprietary product of Mellanox Technologies Ltd.
+#        (the Company) and all right, title, and interest in and to the software product,
+#        including all associated intellectual property rights, are and shall
+#        remain exclusively with the Company.
+#
+#        This software product is governed by the End User License Agreement
+#        provided with the software product.
+#
+#@author: Avner BenHanoch
+
+#@date: 05July2015
+#
+# This script performs blocking connect to a given TCP server
+# It can serve for checking VMA behaviour with a blocking connect when the server
+# is up or down
+#
+import socket, select, os, time, sys, fcntl
+
+def is_connected( sock ):
+	"use poll with zero timeout for checking if socket is writeable and has no errors"
+
+	poller = select.poll()
+	poller.register(sock, select.POLLOUT)
+	pevents = poller.poll(0)
+	rv = True;
+	if len(pevents) == 0: rv = False
+	else:
+		fd, flag = pevents[0]
+		rv = (flag == select.POLLOUT) # we only asked for POLLOUT, verify that we didn't get also errors
+
+	print "- poll returned: %s (SUCCESS=%s)" % (str(pevents), str(rv))
+	return rv
+
+def 	syncronized_echo_client(argv):
+	if (len(argv) <4):
+	    print "Incorrect parameter : " + argv[0] + " server-ip server-port msg-for-echo [bind-IP]"
+	    sys.exit(-1)
+
+	IP   = argv[1]
+	PORT = int(argv[2])
+	msg  = argv[3]
+
+	BIND_IP=None
+
+	if (len(argv) > 4):
+	    BIND_IP=argv[4]
+
+	sock=None
+	sock_fd = 0
+
+	counter = 0
+	success = False
+	while not success:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock_fd = sock.fileno()
+		sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+		flags = fcntl.fcntl(sock_fd, fcntl.F_GETFL, 0)
+		flags = flags & ~os.O_NONBLOCK # set blocking
+		rv = fcntl.fcntl(sock_fd, fcntl.F_SETFL, flags)
+
+		if (BIND_IP): sock.bind((BIND_IP, 0))
+
+		print "starting synchronized connect..."
+		err = sock.connect_ex((IP, PORT))
+		if err != 0:
+			print "error %d"%err
+
+		if is_connected( sock ):
+			success = True
+		else:
+			print counter, "connection was NOT established successfully (will retry in 1 second) - Is the server up?"
+			sock.close()
+			time.sleep(1)
+			counter += 1
+
+	print " **** connection established successfully after %d failures" % counter
+
+	print "* sending..."
+	sock.send(msg)
+
+	print "* sleeping for 1 second"
+	time.sleep (1)
+
+	print "* receiving..."
+	data = sock.recv(1024)
+	print ' **** Received:', data
+
+	print "starting disconnect..."
+	sock.close()
+	print "Done..."
+
+def main():
+	syncronized_echo_client(sys.argv) # for functionality test purposes
+
+if __name__ == "__main__":
+    main()
