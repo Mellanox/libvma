df3d7eeca29fb8d1317faf9c6f0c5d4498f0cd0c
Avner BenHanoch <avnerb@mellanox.com>
2015-07-30 11:30:06 +0300

    issue: 565962 Wrong TCP backlog handling for offloaded sockets by VMA - create an elegant class that is based on a union for for combining key of a connection  (the peer's ip and port) into a single primitive uint64_t. - This will enable fast and elegant insertion of connections' keys into containers such as map or hash table  with no need to create any custom method/operator such as operator< with map, or  operator= and hash/operator size_t() methods with hash table
    Change-Id: I42d454de866243d8f5651793bcf4631c58523aa1

---
+++ src/vma/proto/peer_key.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) Mellanox Technologies Ltd. 2001-2013.  ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of Mellanox Technologies Ltd.
+ * (the "Company") and all right, title, and interest in and to the software product,
+ * including all associated intellectual property rights, are and shall
+ * remain exclusively with the Company.
+ *
+ * This software is made available under either the GPL v2 license or a commercial license.
+ * If you wish to obtain a commercial license, please contact Mellanox at support@mellanox.com.
+ */
+#ifndef __PEER_KEY_H__
+#define __PEER_KEY_H__
+
+/**
+ * Use union for representing ip:port as one uint64_t primitive,
+ *
+ * NOTE: this type provides automatic silent cast to uint64_t.  Hence, it natively supports containers such as map and hash.
+ * NO NEED to implement various operators (such as operator size_t(), operator==, operator<) for map and hash
+ */
+union peer_key {
+public:
+	peer_key(uint32_t _ip, uint16_t _port) : ip(_ip), port(_port){}
+	operator uint64_t() const {return key;} // this will save the need for operator< and for operator== and for operator size_t() with map/hash
+
+private:
+	struct {
+		uint32_t ip;
+		uint16_t port;
+	};
+
+	uint64_t key;
+};
+
+#endif /* ! __PEER_KEY_H__ */
