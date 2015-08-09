7338ea8232233a8b03e0f52fb11d712560418960
Avner BenHanoch <avnerb@mellanox.com>
2015-08-09 12:19:16 +0300

    issue: 565962 1) better making sure peer_key is initialized correctly, 2) comments
    Change-Id: I2900d07bff9f339a6d0d50d729ca31daee56642e

--- src/vma/proto/peer_key.h
+++ src/vma/proto/peer_key.h
@@ -15,20 +15,19 @@
 /**
  * Use union for representing ip:port as one uint64_t primitive,
  *
- * NOTE: this type provides automatic silent cast to uint64_t.  Hence, it natively supports containers such as map and hash.
- * NO NEED to implement various operators (such as operator size_t(), operator==, operator<) for map and hash
+ * NOTE: this type provides implicit cast to uint64_t. Hence, it natively supports containers such as map and hash.
  */
 union peer_key {
 public:
-	peer_key(uint32_t _ip, uint16_t _port) : key(0), ip(_ip), port(_port){}
-	operator uint64_t() const {return key;} // this will save the need for operator< and for operator== and for operator size_t() with map/hash
+	peer_key(uint32_t _ip, uint16_t _port) : ip(_ip), port(_port){}
+	operator uint64_t() const {return key;} // this saves the need for operator< and for operator== and for operator size_t() with map/hash
 
 private:
 	uint64_t key;
 
 	struct {
 		uint32_t ip;
-		uint16_t port;
+		uint32_t port; // 32 bits for making sure all bits of key are initialized
 	};
 };
 
