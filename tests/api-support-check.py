de5e94ce617ab358cd90fc4d707df3b655b925d4
Avner BenHanoch <avnerb@mellanox.com>
2015-10-20 15:22:10 +0200

    issue: 591755 unit also tests fcntl (and light changes in ioctl test) Signed-off-by: Avner BenHanoch <avnerb@mellanox.com>
    Change-Id: Iea8f06569231279b23920e676b150d3f0a511355

--- tests/api-support-check.py
+++ tests/api-support-check.py
@@ -17,20 +17,32 @@
 # This script performs ioctl/fcntl/setsockopt tests for verifying VMA coverage and behavior
 
 # example for usage:
-# LD_PRELOAD=libvma.so VMA_EXCEPTION_HANDLING=2 ./tests/api-support-check.py
+# LD_PRELOAD=libvma.so VMA_EXCEPTION_HANDLING=1 ./tests/api-support-check.py
 #
 import socket, sys, fcntl
-import struct
+import struct, os
 
-def get_ip_address(ifname, sock_type):
-    s = socket.socket(socket.AF_INET, sock_type)
+def get_ip_address(ifname, sock):
     return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
+        sock.fileno(),
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', ifname)
     )[20:24])
 
+def test_fcntl(sock):
+	rv = fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NDELAY)
+
+	lockdata = struct.pack('hhllhh', fcntl.F_WRLCK, 0, 0, 0, 0, 0)
+	rv = fcntl.fcntl(sock, fcntl.F_SETLKW, lockdata)
+	return "fcntl returned with data of len=" + str(len(rv))
+
 if __name__ == "__main__":
     ifname = 'eth0'
-    print "test using UDP: %s=%s" % (ifname, get_ip_address(ifname, socket.SOCK_DGRAM))
-    print "test using TCP: %s=%s" % (ifname, get_ip_address(ifname, socket.SOCK_STREAM))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print "test ioctl using UDP: %s=%s" % (ifname, get_ip_address(ifname, s))
+    print "test fcntl using UDP: " + test_fcntl(s)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print "test ioctl using TCP: %s=%s" % (ifname, get_ip_address(ifname, s))
+    print "test fcntl using TCP: " + test_fcntl(s)
