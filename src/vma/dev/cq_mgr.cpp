19e72f3c05768f2975d418113e118af1322397d1
Avner BenHanoch <avnerb@mellanox.com>
2015-12-31 14:22:59 +0200

    issue: 541581 louder debug log of dropped rx packet, espicially on bad rx_csum
    Change-Id: I359a68927f03066a087f8b19159b9a274698e1b6
    Signed-off-by: Avner BenHanoch <avnerb@mellanox.com>

--- src/vma/dev/cq_mgr.cpp
+++ src/vma/dev/cq_mgr.cpp
@@ -445,7 +445,9 @@ void cq_mgr::process_cq_element_log_helper(mem_buf_desc_t* p_mem_buf_desc, vma_i
 	BULLSEYE_EXCLUDE_BLOCK_START
 	// wce with bad status value
 	if (p_wce->status == IBV_WC_SUCCESS) {
-		cq_logdbg("wce: wr_id=%#x, status=%#x, vendor_err=%#x, qp_num=%#x, csum_ok=%d", p_wce->wr_id, p_wce->status, p_wce->vendor_err, p_wce->qp_num, (!m_b_is_rx_csum_on || vma_wc_rx_csum_ok(*p_wce)));
+		cq_logdbg("wce: wr_id=%#x, status=%#x, vendor_err=%#x, qp_num=%#x", p_wce->wr_id, p_wce->status, p_wce->vendor_err, p_wce->qp_num);
+		if (m_b_is_rx_csum_on && ! vma_wc_rx_csum_ok(*p_wce))
+			cq_logdbg("wce: bad rx_csum");
 		cq_logdbg("wce: opcode=%#x, byte_len=%#d, src_qp=%#x, wc_flags=%#x", vma_wc_opcode(*p_wce), p_wce->byte_len, p_wce->src_qp, vma_wc_flags(*p_wce));
 		cq_logdbg("wce: pkey_index=%#x, slid=%#x, sl=%#x, dlid_path_bits=%#x, imm_data=%#x", p_wce->pkey_index, p_wce->slid, p_wce->sl, p_wce->dlid_path_bits, p_wce->imm_data);
 		cq_logdbg("mem_buf_desc: lkey=%#x, p_buffer=%p, sz_buffer=%#x", p_mem_buf_desc->lkey, p_mem_buf_desc->p_buffer, p_mem_buf_desc->sz_buffer);
@@ -514,6 +516,7 @@ mem_buf_desc_t* cq_mgr::process_cq_element_rx(vma_ibv_wc* p_wce)
 			return NULL;
 		}
 
+		cq_logdbg("wce: bad status or bad rx csum - rx packet will be dropped...");
 		process_cq_element_log_helper(p_mem_buf_desc, p_wce);
 
 		m_p_next_rx_desc_poll = NULL;
