1b6b97f064c13781bc8e541a677a8df6e60af483
Daniel Libenson <daneilli@mellanox.com>
2016-05-30 10:24:43 +0300

    Sigssue: 784234 fix 2: epoll is not cleared from vma_stats Added a NULL pointer check.
    ned-off-by: Daniel Libenson <daneilli@mellanox.com>

--- src/stats/stats_publisher.cpp
+++ src/stats/stats_publisher.cpp
@@ -625,17 +625,23 @@ void vma_stats_instance_remove_epoll_block(iomux_func_stats_t* local_stats_addr)
 {
 	g_lock_ep_stats.lock();
 	iomux_func_stats_t* ep_func_stats = (iomux_func_stats_t*)g_p_stats_data_reader->pop_p_skt_stats(local_stats_addr);
-	if (ep_func_stats) {
-		// Search ep_mem block to release
-		for (int i=0; i<NUM_OF_SUPPORTED_EPFDS; i++) {
-			if (&g_sh_mem->iomux.epoll[i].stats == ep_func_stats) {
-				g_sh_mem->iomux.epoll[i].enabled = false;
-				g_lock_ep_stats.unlock();
-				return;
-			}
+
+	if (NULL == ep_func_stats) {
+		vlog_printf(VLOG_DEBUG, "%s:%d: application vma_stats pointer is NULL\n", __func__, __LINE__);
+		g_lock_ep_stats.unlock();
+		return;
+	}
+
+	// Search ep_mem block to release
+	for (int i=0; i<NUM_OF_SUPPORTED_EPFDS; i++) {
+		if (&g_sh_mem->iomux.epoll[i].stats == ep_func_stats) {
+			g_sh_mem->iomux.epoll[i].enabled = false;
+			g_lock_ep_stats.unlock();
+			return;
 		}
 	}
 
-	vlog_printf(VLOG_WARNING, "Can't remove epoll stat memory block\n");
+	vlog_printf(VLOG_ERROR, "%s:%d: Could not find user pointer (%p)", __func__, __LINE__, ep_func_stats);
 	g_lock_ep_stats.unlock();
+	return;
 }
