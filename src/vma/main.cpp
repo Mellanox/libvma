3280ac282ac4ec20a13214f284e97f9363c46d6e
Ophir Munk <ophirmu@mellanox.com>
2016-07-05 12:13:44 +0300

    issue: 820858 Update message regarding no steering rules in mlx4 to adapt to upstream drivers
    Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

--- src/vma/main.cpp
+++ src/vma/main.cpp
@@ -269,8 +269,8 @@ void check_debug()
 void check_flow_steering_log_num_mgm_entry_size()
 {
 	char flow_steering_val[2] = {0};
-	if (priv_read_file((const char*)FLOW_STEERING_MGM_ENTRY_SIZE_PARAM_FILE, flow_steering_val, 2, VLOG_DEBUG) == -1) {
-		vlog_printf(VLOG_DEBUG, "Flow steering option does not exist in current OFED version");
+	if (priv_try_read_file((const char*)FLOW_STEERING_MGM_ENTRY_SIZE_PARAM_FILE, flow_steering_val, 2) == -1) {
+		vlog_printf(VLOG_DEBUG, "Flow steering option for mlx4 driver does not exist in current OFED version");
 	}
 	else if (flow_steering_val[0] != '-' || flow_steering_val[1] != '1') {
 		vlog_printf(VLOG_WARNING, "***************************************************************************************\n");
