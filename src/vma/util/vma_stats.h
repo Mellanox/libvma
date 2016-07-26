bd9cb24eb96f7f26ad9d1376c4465b91aa6023d2
Liran Oz <lirano@mellanox.com>
2016-07-26 18:22:13 +0300

    issue: 825888 Dumping fd statistics into VMA logger using vma_stats
    Replace fd_to_dump variable position in sh_mem_t struct.
    
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- src/vma/util/vma_stats.h
+++ src/vma/util/vma_stats.h
@@ -309,8 +309,8 @@ typedef struct sh_mem_t {
 	bpool_instance_block_t		bpool_inst_arr[NUM_OF_SUPPORTED_BPOOLS];
 	mc_grp_info_t			mc_info;
 	iomux_stats_t                   iomux;
-	socket_instance_block_t  	skt_inst_arr[]; //sockets statistics array
 	int				fd_to_dump;
+	socket_instance_block_t  	skt_inst_arr[]; //sockets statistics array
 } sh_mem_t;
 
 typedef struct sh_mem_info {
