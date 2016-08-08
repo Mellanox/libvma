04c041bf9e2f7bff6bf70a918f89f5cae1a85c72
Igor Ivanov <igor.ivanov.va@gmail.com>
2016-08-08 13:57:18 +0300

    issue: 841302 Cleanup coverity check
    - Remove useless coverity configuration file
    - Remove make coverity rule becasue it has hardcoded path (use jenkins
    script manually to check)
    
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

--- contrib/jenkins_tests/cov.sh
+++ contrib/jenkins_tests/cov.sh
@@ -34,7 +34,7 @@ for excl in $cov_exclude_file_list; do
     sleep 1
 done
 
-eval "cov-analyze --enable-fnptr --fnptr-models --all --paths 20000 --dir $cov_build --config ${WORKSPACE}/coverity_vma_config.xml"
+eval "cov-analyze --enable-fnptr --fnptr-models --all --paths 20000 --dir $cov_build"
 rc=$(($rc+$?))
 
 set -eE
