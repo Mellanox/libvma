6ccabeca9ab2745a900b3d546f85e3b624c8a3f3
Liran Oz <lirano@mellanox.com>
2016-08-11 16:27:08 +0300

    issue: 844374 Fix error handling while registering a socket with plural epoll instances
    VMA does not supports registration of a socket with plural epoll instances.
    
    Instead of ignoring the request without returning an error, now VMA will
    return with failure and errno set to:
    - EEXIST if the fd is already registered with the same epoll
    - ENOMEM if the fd is already registered with a different epoll

--- src/vma/sock/socket_fd_api.h
+++ src/vma/sock/socket_fd_api.h
@@ -189,8 +189,9 @@ public:
 
 	virtual void consider_rings_migration() {}
 
-	virtual void add_epoll_context(epfd_info *epfd);
+	virtual int add_epoll_context(epfd_info *epfd);
 	virtual void remove_epoll_context(epfd_info *epfd);
+	int get_epoll_context_fd();
 
 #if _BullseyeCoverage
     #pragma BullseyeCoverage off
@@ -222,7 +223,6 @@ protected:
 	void notify_epoll_context_remove_ring(ring* ring);
 	bool notify_epoll_context_verify(epfd_info *epfd);
 	void notify_epoll_context_fd_is_offloaded();
-	int get_epoll_context_fd();
 
 	// identification information <socket fd>
 	int m_fd;
---
+++ tests/functionality/iomux/1epoll_1socket_twice.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <errno.h>
+
+#define MAXEVENTS 64
+
+static int make_socket_non_blocking(int sfd)
+{
+	int flags, s;
+
+	flags = fcntl(sfd, F_GETFL, 0);
+	if (flags == -1) {
+		perror("fcntl");
+		return -1;
+	}
+
+	flags |= O_NONBLOCK;
+	s = fcntl(sfd, F_SETFL, flags);
+	if (s == -1) {
+		perror("fcntl");
+		return -1;
+	}
+
+	return 0;
+}
+
+static int create_and_bind(char *port)
+{
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
+	int s, sfd;
+
+	memset(&hints, 0, sizeof (struct addrinfo));
+	hints.ai_family = AF_UNSPEC;     /* Return IPv4 and IPv6 choices */
+	hints.ai_socktype = SOCK_STREAM; /* We want a TCP socket */
+	hints.ai_flags = AI_PASSIVE;     /* All interfaces */
+
+	s = getaddrinfo(NULL, port, &hints, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror (s));
+		return -1;
+	}
+
+	for (rp = result; rp != NULL; rp = rp->ai_next)	{
+		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (sfd == -1)
+			continue;
+
+		s = bind(sfd, rp->ai_addr, rp->ai_addrlen);
+		if (s == 0) {
+			/* We managed to bind successfully! */
+			break;
+		}
+
+		close(sfd);
+	}
+
+	if (rp == NULL) {
+		fprintf(stderr, "Could not bind\n");
+		return -1;
+	}
+
+	freeaddrinfo(result);
+
+	return sfd;
+}
+
+int main(int argc, char *argv[])
+{
+	int sfd, s;
+	int efd;
+	struct epoll_event event;
+	struct epoll_event *events;
+
+	sfd = create_and_bind((char*)"6666");
+	if (sfd == -1)
+		goto failure;
+
+	printf("--> create socket %d\n", sfd);
+
+	s = make_socket_non_blocking(sfd);
+	if (s == -1)
+		goto failure;
+
+	printf("--> set socket %d non blocking\n", sfd);
+
+	s = listen(sfd, SOMAXCONN);
+	if (s == -1) {
+		perror("--> listen");
+		goto failure;
+	}
+
+	efd = epoll_create1(0);
+	if (efd == -1) {
+		perror("--> epoll_create");
+		goto failure;
+	}
+
+	printf("--> create epoll %d\n",efd);
+
+	event.data.fd = sfd;
+	event.events = EPOLLIN | EPOLLET;
+	s = epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event);
+	if (s == -1) {
+		perror("--> epoll_ctl");
+		goto failure;
+	}
+
+	printf("--> socket %d was registered to epoll %d\n", sfd, efd);
+
+	s = epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event);
+	if (s == -1) {
+		if (errno == EEXIST) {
+			printf("--> socket %d was already registered to epoll %d, errno = %d\n", sfd, efd, errno);
+			printf("--> SUCCESS\n");
+			return EXIT_SUCCESS;
+		} else {
+			printf("--> socket %d was already registered to epoll %d, errno should be set to EEXIST, errno = %d\n", sfd, efd, errno);
+			goto failure;
+		}
+	}
+
+failure:
+	printf("--> FAILURE\n");
+	return EXIT_FAILURE;
+}
