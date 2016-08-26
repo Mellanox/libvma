beffeca53819b4299e9480669775492f63d11d13
Igor Ivanov <igor.ivanov.va@gmail.com>
2016-08-26 19:35:35 +0300

    issue: 770746 Add csbuild support in jenkins
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

--- contrib/jenkins_tests/globals.sh
+++ contrib/jenkins_tests/globals.sh
@@ -24,6 +24,7 @@ test_dir=${WORKSPACE}/${prefix}/test
 rpm_dir=${WORKSPACE}/${prefix}/rpm
 cov_dir=${WORKSPACE}/${prefix}/cov
 cppcheck_dir=${WORKSPACE}/${prefix}/cppcheck
+csbuild_dir=${WORKSPACE}/${prefix}/csbuild
 vg_dir=${WORKSPACE}/${prefix}/vg
 style_dir=${WORKSPACE}/${prefix}/style
 
