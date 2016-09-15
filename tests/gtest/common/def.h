8054849140ff93a3726fe019f6710331f4e81967
Igor Ivanov <igor.ivanov.va@gmail.com>
2016-09-15 16:30:02 +0300

    issue: 824880 Add Google Test based infrastructure
    Current gtest suite includes tcp, udp and vma suites:
    - tcp socket, bind and event test cases
    - vma none
    - udp none
    
    How to build:
    make tests
    
    How to run:
    This test should use two different NICs on the same node
    --addr=<server ip>:<client ip>:<any remote ip>
    LD_PRELOAD=<path>/libvma.so ./gtest --addr=4.4.4.16:1.1.1.16:4.4.4.15
    
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

---
+++ tests/gtest/common/def.h
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2016 Mellanox Technologies, Ltd. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef TESTS_GTEST_COMMON_DEF_H_
+#define TESTS_GTEST_COMMON_DEF_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>   /* printf PRItn */
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/epoll.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <ifaddrs.h>
+#include <pthread.h>
+#include <inttypes.h>   /* printf PRItn */
+#include <fcntl.h>
+#include <poll.h>
+#include <ctype.h>
+#include <malloc.h>
+#include <math.h>
+#include <complex.h>
+#include <time.h>
+#include <signal.h>
+
+
+#include "common/gtest.h" /* Google framework header */
+
+#define INLINE  __inline
+
+#ifndef UNREFERENCED_PARAMETER
+#define UNREFERENCED_PARAMETER(P) ((void)P)
+#endif
+
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
+
+
+/* Platform specific 16-byte alignment macro switch.
+   On Visual C++ it would substitute __declspec(align(16)).
+   On GCC it substitutes __attribute__((aligned (16))).
+*/
+
+#if defined(_MSC_VER)
+#define ALIGN(x) __declspec(align(x))
+#else
+#define ALIGN(x) __attribute__((aligned (x)))
+#endif
+
+#if !defined( EOK )
+#define EOK 0         /* no error */
+#endif
+
+#ifndef container_of
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) (type *)((char *)(ptr) - offsetof(type,member))
+#endif
+
+#define UNDEFINED_VALUE (-1)
+
+struct gtest_configure_t {
+	int log_level;
+	int random_seed;
+	struct sockaddr_in client_addr;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in remote_addr;
+	uint16_t port;
+};
+
+#endif /* TESTS_GTEST_COMMON_DEF_H_ */
---
+++ tests/gtest/tcp/tcp_event.cc
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) 2016 Mellanox Technologies, Ltd. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "common/def.h"
+#include "common/log.h"
+#include "common/sys.h"
+#include "common/base.h"
+
+#include "tcp_base.h"
+
+static void _proc_server(void *ptr);
+static void _proc_client(void *ptr);
+
+class tcp_event : public tcp_base {};
+
+TEST_F(tcp_event, DISABLED_ti_1) {
+	int rc = EOK;
+	int fd;
+	int efd;
+	struct epoll_event event;
+
+	fd = tcp_base::sock_create_nb();
+	ASSERT_LE(0, fd);
+
+	rc = connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	ASSERT_EQ(EINPROGRESS, errno);
+	ASSERT_EQ((-1), rc);
+
+	event.events = 0;
+	event.data.fd = fd;
+	efd = test_base::event_wait(&event);
+	EXPECT_LE(0, efd);
+/*	EXPECT_EQ(EPOLLHUP, event.events); TODO: UNDER VMA */
+	EXPECT_EQ(EPOLLERR | EPOLLHUP, event.events);
+
+	close(efd);
+	close(fd);
+}
+
+TEST_F(tcp_event, ti_2) {
+	int rc = EOK;
+	int fd;
+	int efd;
+	struct epoll_event event;
+
+	fd = tcp_base::sock_create_nb();
+	ASSERT_LE(0, fd);
+
+	rc = connect(fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
+	ASSERT_EQ(EINPROGRESS, errno);
+	ASSERT_EQ((-1), rc);
+
+	event.events = 0;
+	event.data.fd = fd;
+	efd = test_base::event_wait(&event);
+	EXPECT_LE(0, efd);
+	EXPECT_EQ((EPOLLERR | EPOLLHUP), event.events);
+
+	close(efd);
+	close(fd);
+}
+
+TEST_F(tcp_event, DISABLED_ti_3) {
+	int rc = EOK;
+	int fd;
+	int efd;
+	struct epoll_event event;
+
+	fd = tcp_base::sock_create_nb();
+	ASSERT_LE(0, fd);
+
+	rc = connect(fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+	ASSERT_EQ(EINPROGRESS, errno);
+	ASSERT_EQ((-1), rc);
+
+	event.events = EPOLLOUT | EPOLLIN;
+	event.data.fd = fd;
+	efd = test_base::event_wait(&event);
+	EXPECT_LE(0, efd);
+	/*	EXPECT_EQ((EPOLLHUP | EPOLLIN), event.events); TODO: UNDER VMA */
+	EXPECT_EQ((EPOLLERR | EPOLLHUP | EPOLLOUT | EPOLLIN), event.events);
+
+	close(efd);
+	close(fd);
+}
+
+TEST_F(tcp_event, DISABLED_ti_4) {
+	int rc = EOK;
+	int fd;
+	int efd;
+	struct epoll_event event;
+
+	fd = tcp_base::sock_create_nb();
+	ASSERT_LE(0, fd);
+
+	rc = connect(fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr));
+	ASSERT_EQ(EINPROGRESS, errno);
+	ASSERT_EQ((-1), rc);
+
+	event.events = EPOLLOUT | EPOLLIN;
+	event.data.fd = fd;
+	efd = test_base::event_wait(&event);
+	EXPECT_LE(0, efd);
+	/*	EXPECT_EQ((EPOLLERR | EPOLLHUP | EPOLLIN), event.events); TODO: UNDER VMA */
+	EXPECT_EQ((EPOLLERR | EPOLLHUP | EPOLLOUT | EPOLLIN), event.events);
+
+	close(efd);
+	close(fd);
+}
+
+static void _proc_server(void *ptr)
+{
+	int rc = EOK;
+	int fd;
+	int fd_peer;
+	struct sockaddr peer_addr;
+	socklen_t socklen;
+
+	UNREFERENCED_PARAMETER(ptr);
+
+	fd = tcp_base::sock_create();
+	ASSERT_LE(0, fd);
+
+	rc = bind(fd, (struct sockaddr *)&gtest_conf.server_addr, sizeof(gtest_conf.server_addr));
+	ASSERT_EQ(EOK, errno);
+	ASSERT_EQ(0, rc);
+
+	rc = listen(fd, 5);
+	ASSERT_EQ(EOK, errno);
+	ASSERT_EQ(0, rc);
+
+	socklen = sizeof(peer_addr);
+	fd_peer = accept(fd, &peer_addr, &socklen);
+	EXPECT_EQ(EOK, errno);
+	EXPECT_LE(0, fd_peer);
+	EXPECT_EQ(sizeof(peer_addr), socklen);
+
+	log_trace("Accepted connection: fd=%d from %s\n",
+			fd_peer, sys_addr2str((struct sockaddr_in *) &peer_addr));
+
+	close(fd_peer);
+	close(fd);
+}
+
+static void _proc_client(void *ptr)
+{
+	int rc = EOK;
+	int fd;
+	int efd;
+	struct epoll_event event;
+
+	UNREFERENCED_PARAMETER(ptr);
+
+	fd = tcp_base::sock_create_nb();
+	ASSERT_LE(0, fd);
+
+	rc = bind(fd, (struct sockaddr *)&gtest_conf.client_addr, sizeof(gtest_conf.client_addr));
+	ASSERT_EQ(EOK, errno);
+	ASSERT_EQ(0, rc);
+
+	rc = connect(fd, (struct sockaddr *)&gtest_conf.server_addr, sizeof(gtest_conf.server_addr));
+	ASSERT_EQ(EINPROGRESS, errno);
+	ASSERT_EQ((-1), rc);
+
+	event.events = EPOLLOUT | EPOLLIN;
+	event.data.fd = fd;
+	efd = test_base::event_wait(&event);
+	EXPECT_LE(0, efd);
+	EXPECT_EQ((EPOLLOUT), event.events);
+	close(efd);
+
+	log_trace("Established connection: fd=%d to %s\n",
+			fd, sys_addr2str((struct sockaddr_in *) &gtest_conf.server_addr));
+
+	close(fd);
+}
+
+TEST_F(tcp_event, DISABLED_ti_5) {
+	pthread_t server_thread = 0;
+	pthread_t client_thread = 0;
+
+	pthread_create(&server_thread, NULL, (void* (*)(void*))_proc_server, NULL);
+	sleep(1);
+	pthread_create(&client_thread, NULL, (void* (*)(void*))_proc_client, NULL);
+
+	pthread_join(server_thread, NULL);
+	pthread_join(client_thread, NULL);
+}
