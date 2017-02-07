4a508902bfb2b1d2d16babf498da875be6c06cac
Oleg Kuporosov <olegk@mellanox.com>
2017-02-07 05:35:53 +0000

    issue: 968200 FlowTag support for vmapoll.
    Implementation highlights:
    - dynamic probing for flow_tag support on port initialization;
    - fast path for UC TCP/UPD bypasses most of packet checks and RFS dispatch
    - using socket fd+1 as the flow_tag_id. This trivial mapping assists in
      maximizing performance
      (fd=0 is a valid file descriptor but 0 is not a valid flow_tag_id. Hence
      we use fd+1 as the flow_tag_id, effectively limiting our valid range to
      FLOW_TAG_MASK-1);
    
    We would check and ignore flow_tag_id set by adapter in case some
    rule has disabled flow_tag support for this socket for "fast path".
    Such packets still may be valid for adapters without flow_tag_id
    support or multicast cases. So it will require additional full
    check performed by normal data path.
    
    This commit has disabling flow_tag support for multicast flows, it is
    partially enabled in separate commit.
    
    It also has disabled flow_tag support for non vmapoll modes.
    
    Signed-off-by: Oleg Kuporosov <olegk@mellanox.com>

--- src/vma/dev/rfs_uc.cpp
+++ src/vma/dev/rfs_uc.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001-2016 Mellanox Technologies, Ltd. All rights reserved.
+ * Copyright (c) 2001-2017 Mellanox Technologies, Ltd. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -40,7 +40,8 @@
 #define MODULE_NAME 		"rfs_uc"
 
 
-rfs_uc::rfs_uc(flow_tuple *flow_spec_5t, ring_simple *p_ring, rfs_rule_filter* rule_filter /*= NULL*/) : rfs(flow_spec_5t, p_ring, rule_filter)
+rfs_uc::rfs_uc(flow_tuple *flow_spec_5t, ring_simple *p_ring, rfs_rule_filter* rule_filter, uint32_t flow_tag_id) :
+	rfs(flow_spec_5t, p_ring, rule_filter, flow_tag_id)
 {
 	BULLSEYE_EXCLUDE_BLOCK_START
 	if (m_flow_tuple.is_udp_mc()) {
@@ -67,6 +68,7 @@ bool rfs_uc::prepare_flow_spec()
 	attach_flow_data_eth_ipv4_tcp_udp_t*   attach_flow_data_eth = NULL;
 	vma_ibv_flow_spec_ipv4*             p_ipv4 = NULL;
 	vma_ibv_flow_spec_tcp_udp*          p_tcp_udp = NULL;
+	vma_ibv_flow_spec_action_tag*       p_flow_tag = NULL;
 
 	switch (type) {
 		case VMA_TRANSPORT_IB:
@@ -84,12 +86,11 @@ bool rfs_uc::prepare_flow_spec()
 			attach_flow_data_eth = new attach_flow_data_eth_ipv4_tcp_udp_t(m_p_ring->m_p_qp_mgr);
 
 			ibv_flow_spec_eth_set(&(attach_flow_data_eth->ibv_flow_attr.eth),
-					m_p_ring->m_p_l2_addr->get_address(),
+						m_p_ring->m_p_l2_addr->get_address(),
 						htons(m_p_ring->m_p_qp_mgr->get_partiton()));
-
-
 			p_ipv4 = &(attach_flow_data_eth->ibv_flow_attr.ipv4);
 			p_tcp_udp = &(attach_flow_data_eth->ibv_flow_attr.tcp_udp);
+			p_flow_tag = &(attach_flow_data_eth->ibv_flow_attr.flow_tag);
 			p_attach_flow_data = (attach_flow_data_t*)attach_flow_data_eth;
 			break;
 		BULLSEYE_EXCLUDE_BLOCK_START
@@ -112,6 +113,14 @@ bool rfs_uc::prepare_flow_spec()
 		// set priority of 5-tuple to be higher than 3-tuple
 		// to make sure 5-tuple have higher priority on ConnectX-4
 		p_attach_flow_data->ibv_flow_attr.priority = 0;
+	}
+
+	if (m_flow_tag_id && attach_flow_data_eth) { // Will not attach flow_tag spec to rule for tag_id==0
+		ibv_flow_spec_flow_tag_set(p_flow_tag, m_flow_tag_id);
+		attach_flow_data_eth->ibv_flow_attr.add_flow_tag_spec();
+                rfs_logdbg("Adding flow_tag spec to rule, num_of_specs: %d flow_tag_id: %d",
+			   attach_flow_data_eth->ibv_flow_attr.attr.num_of_specs,
+			   m_flow_tag_id);
 	}
 
 	m_attach_flow_data_vector.push_back(p_attach_flow_data);
@@ -147,7 +156,6 @@ bool rfs_uc::rx_dispatch_packet(mem_buf_desc_t* p_rx_wc_buf_desc, void* pv_fd_re
 			}
 		}
 	}
-
 	// Reuse this data buffer & mem_buf_desc
 	return false;
 }
