f2c4cef976082a6bd40b5acdaa0ac9846c753305
Rafi Wiener <rafiw@mellanox.com>
2017-02-26 15:20:30 +0000

    BufferPool: remove allocation logic and switch to vma_allocator
    Signed-off-by: Rafi Wiener <rafiw@mellanox.com>

--- src/vma/dev/buffer_pool.cpp
+++ src/vma/dev/buffer_pool.cpp
@@ -35,8 +35,6 @@
 
 #include <stdlib.h>
 #include <sys/param.h> // for MIN
-#include <sys/shm.h>
-#include <sys/mman.h>
 
 #include "utils/bullseye.h"
 #include "vlogger/vlogger.h"
@@ -78,12 +76,11 @@ void buffer_pool::free_tx_lwip_pbuf_custom(struct pbuf *p_buff)
 }
 
 buffer_pool::buffer_pool(size_t buffer_count, size_t buf_size, ib_ctx_handler *p_ib_ctx_h, mem_buf_desc_owner *owner, pbuf_free_custom_fn custom_free_function) :
-			m_lock_spin("buffer_pool"), m_is_contig_alloc(true), m_shmid(-1), m_p_ib_ctx_h(p_ib_ctx_h),
+			m_lock_spin("buffer_pool"), m_p_ib_ctx_h(p_ib_ctx_h),
 			m_p_head(NULL), m_n_buffers(0), m_n_buffers_created(buffer_count)
 {
 	size_t sz_aligned_element = 0;
 	uint8_t *ptr_buff, *ptr_desc;
-	uint64_t access = VMA_IBV_ACCESS_LOCAL_WRITE;
 
 	__log_info_func("count = %d", buffer_count);
 
@@ -98,66 +95,13 @@ buffer_pool::buffer_pool(size_t buffer_count, size_t buf_size, ib_ctx_handler *p
 	} else {
 		size = buf_size;
 	}
+	void *data_block = m_allocator.allocAndRegMr(size, p_ib_ctx_h);
 
-	//
-	//Huge pages falls back to Contiguous pages that falls back to Anon (Malloc).
-	//
-	switch (safe_mce_sys().mem_alloc_type) {
-	case ALLOC_TYPE_HUGEPAGES:
-		if (!hugetlb_alloc(size)) {
-			__log_info_dbg("Failed allocating huge pages, falling back to contiguous pages");
-		}
-		else {
-			__log_info_dbg("Huge pages allocation passed successfully");
-			if (!register_memory(size, m_p_ib_ctx_h, access)) {
-				__log_info_dbg("failed registering huge pages data memory block");
-				free_bpool_resources();
-				throw_vma_exception("failed registering huge pages data memory block");
-			}
-			break;
-		}
-	case ALLOC_TYPE_CONTIG:
-#ifndef VMA_IBV_ACCESS_ALLOCATE_MR
-		m_is_contig_alloc = false;
-#else
-		m_data_block = NULL;
-		access |= VMA_IBV_ACCESS_ALLOCATE_MR; // for contiguous pages use only
-		if (!register_memory(size, m_p_ib_ctx_h, access)) {
-			__log_info_dbg("Failed allocating contiguous pages");
-			m_is_contig_alloc = false;
-		}
-		else {
-			__log_info_dbg("Contiguous pages allocation passed successfully");
-			break;
-		}
-#endif
-	case ALLOC_TYPE_ANON:
-	default:
-		__log_info_dbg("allocating memory using malloc()");
-#ifdef VMA_IBV_ACCESS_ALLOCATE_MR
-		access &= ~VMA_IBV_ACCESS_ALLOCATE_MR;
-#endif
-		m_data_block = malloc(size);
-		BULLSEYE_EXCLUDE_BLOCK_START
-		if (m_data_block == NULL) {
-			__log_info_dbg("failed allocating data memory block (size=%d Kbytes) (errno=%d %m)",
-					size/1024, errno);
-			free_bpool_resources();
-			throw_vma_exception("failed allocating data memory block");
-		}
-		BULLSEYE_EXCLUDE_BLOCK_END
-		if (!register_memory(size, m_p_ib_ctx_h, access)) {
-			__log_info_dbg("failed registering data memory block");
-			free_bpool_resources();
-			throw_vma_exception("failed registering data memory block");
-		}
-		break;
-	}
 
 	if (!buffer_count) return;
 
 	// Align pointers
-	ptr_buff = (uint8_t *)((unsigned long)((char*)m_data_block + MCE_ALIGNMENT) & (~MCE_ALIGNMENT));
+	ptr_buff = (uint8_t *)((unsigned long)((char*)data_block + MCE_ALIGNMENT) & (~MCE_ALIGNMENT));
 	ptr_desc = ptr_buff + sz_aligned_element * buffer_count;
 
 	// Split the block to buffers
@@ -188,32 +132,12 @@ buffer_pool::~buffer_pool()
 void buffer_pool::free_bpool_resources()
 {
 	if (m_n_buffers == m_n_buffers_created) {
-		__log_info_func("count %lu, missing %lu", m_n_buffers, m_n_buffers_created-m_n_buffers);
+		__log_info_func("count %lu, missing %lu", m_n_buffers,
+				m_n_buffers_created-m_n_buffers);
 	}
 	else {
-		__log_info_dbg("count %lu, missing %lu", m_n_buffers, m_n_buffers_created - m_n_buffers);
-	}
-
-	// Unregister memory
-	std::deque<ibv_mr*>::iterator iter_mrs;
-	for (iter_mrs = m_mrs.begin(); iter_mrs != m_mrs.end(); ++iter_mrs) {
-		ibv_mr *mr = *iter_mrs;
-		ib_ctx_handler* p_ib_ctx_handler = g_p_ib_ctx_handler_collection->get_ib_ctx(mr->context); 
-		if (!p_ib_ctx_handler->is_removed()) {
-			IF_VERBS_FAILURE(ibv_dereg_mr(mr)) {
-				__log_info_err("failed de-registering a memory region (errno=%d %m)", errno);
-			} ENDIF_VERBS_FAILURE;
-		}
-	}
-	// Release memory
-	if (m_shmid >= 0) { // Huge pages mode
-		BULLSEYE_EXCLUDE_BLOCK_START
-		if (m_data_block && (shmdt(m_data_block) != 0)) {
-			__log_info_err("shmem detach failure %m");
-		}
-		BULLSEYE_EXCLUDE_BLOCK_END
-	} else if (!m_is_contig_alloc){ // in contig mode 'ibv_dereg_mr' will free all allocates resources
-		free(m_data_block);
+		__log_info_dbg("count %lu, missing %lu", m_n_buffers,
+				m_n_buffers_created - m_n_buffers);
 	}
 
 	vma_stats_instance_remove_bpool_block(m_p_bpool_stat);
@@ -221,126 +145,6 @@ void buffer_pool::free_bpool_resources()
 	__log_info_func("done");
 }
 
-bool buffer_pool::hugetlb_alloc(size_t sz_bytes)
-{
-	size_t hugepagemask = 4 * 1024 * 1024 - 1;
-	sz_bytes = (sz_bytes + hugepagemask) & (~hugepagemask);
-
-	__log_info_dbg("Allocating %ld bytes in huge tlb", sz_bytes);
-
-	// allocate memory
-	m_shmid = shmget(IPC_PRIVATE, sz_bytes,
-			SHM_HUGETLB | IPC_CREAT | SHM_R | SHM_W);
-	if (m_shmid < 0) {
-
-		// Stop trying to use HugePage if failed even once
-		safe_mce_sys().mem_alloc_type = ALLOC_TYPE_CONTIG;
-
-		vlog_printf(VLOG_WARNING, "**************************************************************\n");
-		vlog_printf(VLOG_WARNING, "* NO IMMEDIATE ACTION NEEDED!                                 \n");
-		vlog_printf(VLOG_WARNING, "* Not enough hugepage resources for VMA memory allocation.    \n");
-		vlog_printf(VLOG_WARNING, "* VMA will continue working with regular memory allocation.   \n");
-		vlog_printf(VLOG_INFO, "   * Optional:                                                   \n");
-		vlog_printf(VLOG_INFO, "   *   1. Switch to a different memory allocation type           \n");
-		vlog_printf(VLOG_INFO, "   *      (%s= 0 or 1)                                           \n", SYS_VAR_MEM_ALLOC_TYPE);
-		vlog_printf(VLOG_INFO, "   *   2. Restart process after increasing the number of         \n");
-		vlog_printf(VLOG_INFO, "   *      hugepages resources in the system:                     \n");
-		vlog_printf(VLOG_INFO, "   *      \"cat /proc/meminfo |  grep -i HugePage\"              \n");
-		vlog_printf(VLOG_INFO, "   *      \"echo 1000000000 > /proc/sys/kernel/shmmax\"          \n");
-		vlog_printf(VLOG_INFO, "   *      \"echo 800 > /proc/sys/vm/nr_hugepages\"               \n");
-		vlog_printf(VLOG_WARNING, "* Please refer to the memory allocation section in the VMA's  \n");
-		vlog_printf(VLOG_WARNING, "* User Manual for more information                            \n");
-		vlog_printf(VLOG_WARNING, "***************************************************************\n");
-		return false;
-	}
-
-	// get pointer to allocated memory
-	m_data_block = shmat(m_shmid, NULL, 0);
-	if (m_data_block == (void*)-1) {
-		__log_info_warn("Shared memory attach failure (errno=%d %m)", errno);
-		shmctl(m_shmid, IPC_RMID, NULL);
-		m_shmid = -1;
-		m_data_block = NULL;
-		return false;
-	}
-
-	// mark 'to be destroyed' when process detaches from shmem segment
-	// this will clear the HugePage resources even if process if killed not nicely
-	if (shmctl(m_shmid, IPC_RMID, NULL)) {
-		__log_info_warn("Shared memory contrl mark 'to be destroyed' failed (errno=%d %m)", errno);
-	}
-
-	// We want to determine now that we can lock it. Note: it was claimed that without actual mlock, linux might be buggy on this with huge-pages
-	int rc = mlock(m_data_block, sz_bytes);
-	if (rc!=0) {
-		__log_info_warn("mlock of shared memory failure (errno=%d %m)", errno);
-		if (shmdt(m_data_block) != 0) {
-			__log_info_err("shmem detach failure %m");
-		}
-		m_data_block = NULL; // no value to try shmdt later
-		m_shmid = -1;
-		return false;
-	}
-
-	return true;
-}
-
-bool buffer_pool::register_memory(size_t size, ib_ctx_handler *p_ib_ctx_h, uint64_t access)
-{
-	if (p_ib_ctx_h) {
-		ibv_mr *mr = p_ib_ctx_h->mem_reg(m_data_block, size, access);
-		if (mr == NULL){
-			if (m_data_block) {
-				__log_info_warn("Failed registering memory, This might happen due to low MTT entries. Please refer to README.txt for more info");
-				__log_info_dbg("Failed registering memory block with device (ptr=%p size=%ld%s) (errno=%d %m)",
-						m_data_block, size, errno);
-				free_bpool_resources();
-				throw_vma_exception("Failed registering memory block");
-			} else {
-				__log_info_warn("Failed allocating or registering memory in contiguous mode. Please refer to README.txt for more info");
-				return false;
-			}
-		}
-		m_mrs.push_back(mr);
-		if (!m_data_block) { // contig pages mode
-			m_data_block = mr->addr;
-		}
-	}
-	else {
-		size_t num_devices = g_p_ib_ctx_handler_collection->get_num_devices();
-		ibv_mr *mrs[num_devices];
-
-		BULLSEYE_EXCLUDE_BLOCK_START
-		if (g_p_ib_ctx_handler_collection->mem_reg_on_all_devices(m_data_block, size, mrs,
-				num_devices, access) != num_devices) {
-			if (m_data_block) {
-				__log_info_warn("Failed registering memory, This might happen due to low MTT entries. Please refer to README.txt for more info");
-				__log_info_dbg("Failed registering memory block with device (ptr=%p size=%ld%s) (errno=%d %m)",
-						m_data_block, size, errno);
-				free_bpool_resources();
-				throw_vma_exception("Failed registering memory");
-			} else {
-				__log_info_warn("Failed allocating or registering memory in contiguous mode. Please refer to README.txt for more info");
-				return false;
-			}
-		}
-		BULLSEYE_EXCLUDE_BLOCK_END
-
-		if (!m_data_block) { // contig pages mode
-			m_data_block = mrs[0]->addr;
-			if (!m_data_block) {
-				__log_info_dbg("Failed registering memory, check that OFED is loaded successfully");
-				free_bpool_resources();
-				throw_vma_exception("Failed registering memory");
-			}
-		}
-		for (size_t i = 0; i < num_devices; ++i) {
-			m_mrs.push_back(mrs[i]);
-		}
-	}
-
-	return true;
-}
 
 mem_buf_desc_t *buffer_pool::get_buffers(size_t count, uint32_t lkey)
 {
@@ -397,18 +201,7 @@ mem_buf_desc_t *buffer_pool::get_buffers_thread_safe(size_t count, uint32_t lkey
 
 inline uint32_t buffer_pool::find_lkey_by_ib_ctx(ib_ctx_handler* p_ib_ctx_h)
 {
-	uint32_t lkey = 0;
-	if (likely(p_ib_ctx_h)) {
-		std::deque<ibv_mr*>::iterator iter;
-		for (iter = m_mrs.begin(); iter != m_mrs.end(); ++iter) {
-			ibv_mr *mr = *iter;
-			if (mr->context->device == p_ib_ctx_h->get_ibv_device()) {
-				lkey = mr->lkey;
-				break;
-			}
-		}
-	}
-	return lkey;
+	return m_allocator.find_lkey_by_ib_ctx(p_ib_ctx_h);
 }
 
 uint32_t buffer_pool::find_lkey_by_ib_ctx_thread_safe(ib_ctx_handler* p_ib_ctx_h)
