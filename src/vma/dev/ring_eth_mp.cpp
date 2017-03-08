19a74bc43bc1c5b892aed949216d18b4518ca482
Rafi Wiener <rafiw@mellanox.com>
2017-03-08 13:16:46 +0000

    MP_RQ: add MP_RQ ring ctrl path
    Signed-off-by: Rafi Wiener <rafiw@mellanox.com>

---
+++ src/vma/dev/ring_eth_mp.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2001-2017 Mellanox Technologies, Ltd. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <dev/ring_eth_mp.h>
+#include <dev/qp_mgr_mp.h>
+
+#undef  MODULE_NAME
+#define MODULE_NAME		"ring_eth_mp"
+#undef  MODULE_HDR
+#define MODULE_HDR		MODULE_NAME "%d:%s() "
+
+
+#ifndef DEFINED_IBV_OLD_VERBS_MLX_OFED
+
+ring_eth_mp::ring_eth_mp(in_addr_t local_if,
+			 ring_resource_creation_info_t *p_ring_info, int count,
+			 bool active, uint16_t vlan, uint32_t mtu,
+			 ring *parent) throw (vma_error) :
+			 ring_eth(local_if, p_ring_info, count, active, vlan,
+				  mtu, parent, false),
+			 m_strides_num(9), m_stride_size(7), m_res_domain(NULL),
+			 m_wq_count(40), curr_wq(0), curr_ptr(NULL)
+{
+	// call function from derived not base
+	m_buffer_size = (1 << m_stride_size) * (1 << m_strides_num) *
+			m_wq_count + 1024;
+	create_resources(p_ring_info, active);
+}
+
+void ring_eth_mp::create_resources(ring_resource_creation_info_t *p_ring_info,
+				   bool active) throw (vma_error)
+{
+	struct ibv_exp_res_domain_init_attr res_domain_attr;
+
+	// check MP capabilities currently all caps are 0 due to a buf
+	vma_ibv_device_attr& r_ibv_dev_attr =
+			p_ring_info->p_ib_ctx->get_ibv_device_attr();
+	/*if (!r_ibv_dev_attr.max_ctx_res_domain) {
+		throw_vma_exception("device doesn't support resource domain");
+		return;
+	}
+	if (!(r_ibv_dev_attr.mp_rq_caps.supported_qps & IBV_EXP_QPT_RC)) {
+		throw_vma_exception("device doesn't support RC QP");
+	}*/
+
+	if (m_stride_size <
+		r_ibv_dev_attr.mp_rq_caps.min_single_stride_log_num_of_bytes) {
+		ring_logwarn("stride byte size is to low, supported %d, given %d",
+			     r_ibv_dev_attr.mp_rq_caps.min_single_stride_log_num_of_bytes,
+			     m_stride_size);
+		throw_vma_exception("stride byte size is to low");
+	}
+	/*if (m_stride_size >
+		r_ibv_dev_attr.mp_rq_caps.max_single_stride_log_num_of_bytes) {
+		ring_logwarn("stride byte size is to high, supported %d, given %d",
+			     r_ibv_dev_attr.mp_rq_caps.min_single_stride_log_num_of_bytes,
+			     m_stride_size);
+		throw_vma_exception("stride byte size is to high");
+	}*/
+	if (m_strides_num <
+		r_ibv_dev_attr.mp_rq_caps.min_single_wqe_log_num_of_strides) {
+		ring_logwarn("strides num is to low, supported %d, given %d",
+			     r_ibv_dev_attr.mp_rq_caps.min_single_wqe_log_num_of_strides,
+			     m_strides_num);
+		throw_vma_exception("strides num is to low");
+	}
+	/*if (m_strides_num >
+		r_ibv_dev_attr.mp_rq_caps.max_single_wqe_log_num_of_strides) {
+		ring_logwarn("strides num is to high, supported %d, given %d",
+			     r_ibv_dev_attr.mp_rq_caps.min_single_wqe_log_num_of_strides,
+			     m_strides_num);
+		throw_vma_exception("strides num is to high");
+	}*/
+
+	res_domain_attr.comp_mask = IBV_EXP_RES_DOMAIN_THREAD_MODEL |
+				    IBV_EXP_RES_DOMAIN_MSG_MODEL;
+
+	// in VMA we are in charge of locks!
+	res_domain_attr.thread_model = IBV_EXP_THREAD_UNSAFE;
+
+	// currently have no affect
+	res_domain_attr.msg_model = IBV_EXP_MSG_HIGH_BW;
+
+	m_res_domain = ibv_exp_create_res_domain(
+				p_ring_info->p_ib_ctx->get_ibv_context(),
+				&res_domain_attr);
+	if (!m_res_domain) {
+		throw_vma_exception("failed creating resource domain");
+		return;
+	}
+	// create cyclic buffer get exception on failure
+	alloc.allocAndRegMr(m_buffer_size + MCE_ALIGNMENT + 1,
+			    p_ring_info->p_ib_ctx) ;
+	errno = 0;
+	// point m_sge to buffer
+	int strides_num = 1 << m_strides_num;
+	int strides_length = 1 << m_stride_size;
+
+	// RAFI need to change to nice logic check
+	assert(uint32_t(strides_num * strides_length * m_wq_count) < m_buffer_size);
+	// create ring simple resources
+	ring_simple::create_resources(p_ring_info, active);
+}
+
+qp_mgr* ring_eth_mp::create_qp_mgr(const ib_ctx_handler *ib_ctx,
+				   uint8_t port_num,
+				   struct ibv_comp_channel *p_rx_comp_event_channel) throw (vma_error)
+{
+	return new qp_mgr_mp(this, ib_ctx, port_num, p_rx_comp_event_channel,
+			get_tx_num_wr(), get_partition());
+}
+
+int ring_eth_mp::drain_and_proccess(cq_type_t cq_type)
+{
+	(void)cq_type;
+	return 0;
+}
+
+ring_eth_mp::~ring_eth_mp()
+{
+	struct ibv_exp_destroy_res_domain_attr attr;
+	int res = ibv_exp_destroy_res_domain(
+			m_p_qp_mgr->get_ib_ctx_handler()->get_ibv_context(),
+			m_res_domain,
+			&attr);
+	if (res)
+		ring_logdbg("call to ibv_exp_destroy_res_domain returned %d", res);
+
+}
+#endif
+
--- src/vma/dev/ring_simple.h
+++ src/vma/dev/ring_simple.h
@@ -38,6 +38,8 @@
 #include "vma/util/utils.h"
 #include "vma/vma_extra.h"
 
+class ring_eth_mp;
+
 class ring_simple : public ring
 {
 public:
@@ -84,7 +86,7 @@ public:
 	transport_type_t	get_transport_type() const { return m_transport_type; }
 	virtual bool 		get_hw_dummy_send_support(ring_user_id_t id, vma_ibv_send_wr* p_send_wqe);
 	inline void 		convert_hw_time_to_system_time(uint64_t hwtime, struct timespec* systime) { m_p_cq_mgr_rx->convert_hw_time_to_system_time(hwtime, systime); }
-
+	struct ibv_comp_channel* get_tx_comp_event_channel() { return m_p_tx_comp_event_channel; }
 	friend class cq_mgr;
 	friend class qp_mgr;
 	friend class rfs;
@@ -95,7 +97,7 @@ public:
 
 protected:
 	virtual qp_mgr*		create_qp_mgr(const ib_ctx_handler* ib_ctx, uint8_t port_num, struct ibv_comp_channel* p_rx_comp_event_channel) = 0;
-	void			create_resources(ring_resource_creation_info_t* p_ring_info, bool active) throw (vma_error);
+	virtual void		create_resources(ring_resource_creation_info_t* p_ring_info, bool active) throw (vma_error);
 	// Internal functions. No need for locks mechanism.
 #ifdef DEFINED_VMAPOLL	
 	inline void 		vma_poll_process_recv_buffer(mem_buf_desc_t* p_rx_wc_buf_desc);
@@ -108,15 +110,15 @@ protected:
 	void			flow_udp_mc_del_all();
 	void			flow_tcp_del_all();
 	bool			request_more_tx_buffers(uint32_t count);
-	struct ibv_comp_channel* get_tx_comp_event_channel() { return m_p_tx_comp_event_channel; }
 	uint32_t		get_tx_num_wr() { return m_tx_num_wr; }
 	uint16_t		get_partition() { return m_partition; }
 #ifdef DEFINED_VMAPOLL		
 	uint16_t		get_lkey() { return m_tx_lkey; }
 #endif // DEFINED_VMAPOLL		
 
+	qp_mgr*			m_p_qp_mgr;
 	struct cq_moderation_info m_cq_moderation_info;
-
+	cq_mgr*			m_p_cq_mgr_rx;
 private:
 	inline void		send_status_handler(int ret, vma_ibv_send_wr* p_send_wqe);
 	inline mem_buf_desc_t*	get_tx_buffers(uint32_t n_num_mem_bufs);
@@ -129,8 +131,6 @@ private:
 
 	lock_spin_recursive	m_lock_ring_rx;
 	lock_spin_recursive	m_lock_ring_tx;
-	qp_mgr*			m_p_qp_mgr;
-	cq_mgr*			m_p_cq_mgr_rx;
 	cq_mgr*			m_p_cq_mgr_tx;
 	lock_mutex		m_lock_ring_tx_buf_wait;
 	descq_t			m_tx_pool;
@@ -168,9 +168,14 @@ private:
 class ring_eth : public ring_simple
 {
 public:
-	ring_eth(in_addr_t local_if, ring_resource_creation_info_t* p_ring_info, int count, bool active, uint16_t vlan, uint32_t mtu, ring* parent = NULL) throw (vma_error):
-		ring_simple(local_if, vlan, count, VMA_TRANSPORT_ETH, mtu, parent) { create_resources(p_ring_info, active); };
-
+	ring_eth(in_addr_t local_if, ring_resource_creation_info_t* p_ring_info,
+		 int count, bool active, uint16_t vlan, uint32_t mtu,
+		 ring* parent = NULL, bool call_create_res = true) throw (vma_error):
+		ring_simple(local_if, vlan, count, VMA_TRANSPORT_ETH, mtu, parent) {
+		if (call_create_res)
+			create_resources(p_ring_info, active);
+	};
+	virtual ~ring_eth(){}
 protected:
 	virtual qp_mgr* create_qp_mgr(const ib_ctx_handler* ib_ctx, uint8_t port_num, struct ibv_comp_channel* p_rx_comp_event_channel) throw (vma_error);
 };
