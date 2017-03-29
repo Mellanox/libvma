4e21118caaf22cac56bb5fcc7b59a94344a30458
Liran Oz <lirano@mellanox.com>
2017-03-29 16:39:30 +0300

    issue: 1011829 Fix VMA_TCP_NODELAY parameter setting (Nagle algorithm)
    Enable TCP_NODELAY using setsockopt() in case VMA_TCP_NODELAY
    configuration parameter is enabled
    
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- src/vma/sock/sockinfo_tcp.cpp
+++ src/vma/sock/sockinfo_tcp.cpp
@@ -267,8 +267,12 @@ sockinfo_tcp::sockinfo_tcp(int fd) throw (vma_exception) :
 
 	// Disable Nagle algorithm if VMA_TCP_NODELAY flag was set.
 	if (safe_mce_sys().tcp_nodelay) {
-		tcp_nagle_disable(&m_pcb);
-		fit_snd_bufs_to_nagle(true);
+		try {
+			int tcp_nodelay = 1;
+			setsockopt(IPPROTO_TCP, TCP_NODELAY, &tcp_nodelay, sizeof(tcp_nodelay));
+		} catch (vma_error&) {
+			// We should not be here
+		}
 	}
 
 	si_tcp_logdbg("TCP PCB FLAGS: 0x%x", m_pcb.flags);
