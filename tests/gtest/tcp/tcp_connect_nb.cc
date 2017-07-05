99a48fa64e08779f2f67b8e4bc2cea312391d985
Sergey Lykov <sergeyly@mellanox.com>
2017-07-05 11:51:16 +0300

    issue: 1063164 Update License date
    Signed-off-by: Sergey Lykov <sergeyly@mellanox.com>

--- tests/gtest/tcp/tcp_connect_nb.cc
+++ tests/gtest/tcp/tcp_connect_nb.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Mellanox Technologies, Ltd. All rights reserved.
+ * Copyright (c) 2001-2017 Mellanox Technologies, Ltd. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
