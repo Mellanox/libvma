b65bb9b038e928b43ec822e2bf1e115e53a3d740
Igor Ivanov <igor.ivanov.va@gmail.com>
2017-09-21 14:06:37 +0300

    issue: 1095528 Process SEND_LSO with few sge
    Add ability to process multiple scatter-gather
    elements for LSO/TSO case.
    Implement wrap around support for ethernet and data
    segments.
    Do not use BF for TSO wqe.
    
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

--- src/vma/dev/qp_mgr_eth_mlx5.h
+++ src/vma/dev/qp_mgr_eth_mlx5.h
@@ -40,7 +40,6 @@
 
 #if defined(DEFINED_DIRECT_VERBS)
 
-
 struct mlx5_wqe64 {
 	union {
 		struct mlx5_wqe_ctrl_seg ctrl;
@@ -74,26 +73,26 @@ private:
 	virtual bool	is_completion_need() { return !m_n_unsignaled_count || (m_dm_enabled && m_dm_mgr.is_completion_need()); };
 	virtual void	dm_release_data(mem_buf_desc_t* buff) { m_dm_mgr.release_data(buff); }
 
-	inline void	set_signal_in_next_send_wqe();
 	int		send_to_wire(vma_ibv_send_wr* p_send_wqe, vma_wr_tx_packet_attr attr, bool request_comp);
 	inline int	fill_wqe(vma_ibv_send_wr* p_send_wqe);
 	inline int	fill_wqe_lso(vma_ibv_send_wr* pswr);
-	inline void	ring_doorbell(uint64_t* wqe, int num_wqebb, int num_wqebb_top = 0);
+	inline void	set_signal_in_next_send_wqe();
+	inline void	ring_doorbell(uint64_t* wqe, int db_method, int num_wqebb, int num_wqebb_top = 0);
 	inline int	fill_inl_segment(sg_array &sga, uint8_t *cur_seg, uint8_t* data_addr, int max_inline_len, int inline_len);
 	inline int	fill_ptr_segment(sg_array &sga, struct mlx5_wqe_data_seg* dp_seg, uint8_t* data_addr, int data_len, mem_buf_desc_t* buffer);
 
 	struct mlx5_wqe64	(*m_sq_wqes)[];
 	struct mlx5_wqe64*	m_sq_wqe_hot;
 	uint8_t*		m_sq_wqes_end;
+	enum {
+		MLX5_DB_METHOD_BF,
+		MLX5_DB_METHOD_DB
+	} m_db_method;
 
 	int                 m_sq_wqe_hot_index;
 	uint16_t            m_sq_wqe_counter;
 	dm_mgr              m_dm_mgr;
 	bool                m_dm_enabled;
-	enum {
-		MLX5_DB_METHOD_BF,
-		MLX5_DB_METHOD_DB
-	} m_db_method;
 };
 #endif //defined(DEFINED_DIRECT_VERBS)
 #endif //QP_MGR_ETH_MLX5_H
