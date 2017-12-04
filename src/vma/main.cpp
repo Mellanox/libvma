b3b54b794dd3045216f181c23954679a80ae9ad1
liranoz12 <liranoz12@gmail.com>
2017-12-04 15:53:20 +0200

    issue: 1222660 Correct warning check for flow steering
    Add new line to print

--- src/vma/main.cpp
+++ src/vma/main.cpp
@@ -310,7 +310,7 @@ void check_flow_steering_log_num_mgm_entry_size()
 {
 	char flow_steering_val[4] = {0};
 	if (priv_safe_try_read_file((const char*)FLOW_STEERING_MGM_ENTRY_SIZE_PARAM_FILE, flow_steering_val, sizeof(flow_steering_val)) == -1) {
-		vlog_printf(VLOG_DEBUG, "Flow steering option for mlx4 driver does not exist in current OFED version");
+		vlog_printf(VLOG_DEBUG, "Flow steering option for mlx4 driver does not exist in current OFED version\n");
 	}
 	else if (flow_steering_val[0] != '-' || (strtol(&flow_steering_val[1], NULL, 0) % 2) == 0) {
 		vlog_printf(VLOG_WARNING, "***************************************************************************************\n");
