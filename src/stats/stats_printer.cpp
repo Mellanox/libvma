e568d3e5e5141be56e5b0325f110cd862657d952
Liran Oz <lirano@mellanox.com>
2018-01-18 09:21:13 +0000

    issue: 1272183 Clean stats_printer.cpp
    Remove duplicate code from stats_printer.cpp file.
    
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- src/stats/stats_printer.cpp
+++ src/stats/stats_printer.cpp
@@ -34,34 +34,9 @@
 #include "config.h"
 #endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <fcntl.h>
-#include <errno.h>
-#include <list>
-#include <sys/mman.h>
-#include <sys/resource.h>
-#include <sys/utsname.h>
-#include <sys/stat.h>
-#include <signal.h>
-#include <getopt.h>		/* getopt()*/
-#include <errno.h>
-#include <dirent.h>
-#include <string.h>
-#include <vma/util/utils.h>
-#include <vma/util/vma_stats.h>
-
-using namespace std;
-
-typedef std::list<int> fd_list_t;
-
-
-typedef struct {
-	in_addr_t		mc_grp;
-	fd_list_t		fd_list;
-} mc_group_fds_t;
+#include "vma/util/utils.h"
+#include "vma/util/vma_stats.h"
+#include "vma/lwip/tcp.h"
 
 typedef enum {
 	e_K = 1024,
@@ -70,72 +45,7 @@ typedef enum {
 
 user_params_t user_params;
 
-#define MODULE_NAME				"vmastat"
-#define log_msg(log_fmt, log_args...)		printf(MODULE_NAME  ": " log_fmt "\n", ##log_args)
-#define log_err(log_fmt, log_args...)		fprintf(stderr,MODULE_NAME ": " log_fmt "\n", ##log_args)
-#define log_system_err(log_fmt, log_args...)	fprintf(stderr,MODULE_NAME ": " log_fmt " (errno=%d %s)\n", ##log_args, errno, strerror(errno))
-#define log_dbg(log_fmt, log_args...)		printf(MODULE_NAME ": " log_fmt "\n", ##log_args)
-
-#define VMA_SH_OBJ_DIR			"/tmp"
-
-#define BASE_HEADERS_NUM		2
-#define BASIC_STATS_LINES_NUM		2
-#define	 UPPER_SHORT_VIEW_HEADER	" %-7s %42s %31s\n"
-#define LOWER_SHORT_VIEW_HEADER		" %-7s %10s %7s %8s %7s %6s %7s %7s %7s %7s\n"
-#define RX_SHORT_VIEW			" %-3d %-3s %10u %7u %8u %7u %6.1f %7u %7u %7u %7u\n"
-#define TX_SHORT_VIEW			" %-3s %-3s %10u %7u %8u %7u %-6s %7u %7u %7u %7u\n"
-#define IOMUX_FORMAT			"%-8s%-2s %-9s%u%-1s%u %-12s %-9s%-5u %-7s%-4u %-5s%-2.2f%-3s %-5s%d%-1s\n"
-
-#define MEDIUM_HEADERS_NUM		3
-#define MEDIUM_STATS_LINES_NUM		2
-#define	 UPPER_MEDIUM_VIEW_HEADER	" %-7s %65s %31s\n"
-#define MIDDLE_MEDIUM_VIEW_HEADER	" %-7s %10s %7s %8s %7s %6s%23s %7s %7s %7s %7s\n"
-#define LOWER_MEDIUM_VIEW_HEADER	" %50s %6s  %6s  %6s \n"
-#define RX_MEDIUM_VIEW			" %-3d %-3s %10u %7u %8u %7u %6.1f %6u  %6u  %6u %7u %7u %7u %7u\n"
-#define TX_MEDIUM_VIEW			" %-3s %-3s %10u %7u %8u %7u %29s %7u %7u %7u %7u\n"
-#define CYCLES_SEPARATOR		"-------------------------------------------------------------------------------\n"
-#define FORMAT_CQ_STATS_32bit		"%-20s %10u\n"
-#define FORMAT_CQ_STATS_64bit		"%-20s %10llu %-3s\n"
-#define FORMAT_CQ_STATS_percent		"%-20s %10.2f%%\n"
-
-#define INTERVAL			1
 #define BYTES_TRAFFIC_UNIT		e_K
-#define SCREEN_SIZE			24
-#define MAX_BUFF_SIZE			256
-#define PRINT_DETAILS_MODES_NUM		2
-#define DEFAULT_DELAY_SEC		1
-#define DEFAULT_VIEW_MODE		e_basic
-#define DEFAULT_DETAILS_MODE		e_totals
-#define	 DEFAULT_PROC_IDENT_MODE	e_by_runn_proccess
-#define VLOG_LEVELS_NUM			7
-#define VLOG_DETAILS_NUM		4
-#define INIT_VMA_LOG_LEVEL_VAL		-1
-#define INIT_VMA_LOG_DETAILS		-1
-#define NANO_TO_MICRO(n)		(((n) + 500) / 1000)
-#define SEC_TO_MICRO(n)			((n) * 1000000)
-#define TIME_DIFF_in_MICRO(start,end)	(SEC_TO_MICRO((end).tv_sec-(start).tv_sec) + \
-					(NANO_TO_MICRO((end).tv_nsec-(start).tv_nsec)))
-// printf formating when IP is in network byte ordering (for LITTLE_ENDIAN)
-#define NETWORK_IP_PRINTQUAD_LITTLE_ENDIAN(ip)     		(uint8_t)((ip)&0xff), (uint8_t)(((ip)>>8)&0xff),(uint8_t)(((ip)>>16)&0xff),(uint8_t)(((ip)>>24)&0xff)
-
-// printf formating when IP is in host byte ordering (for LITTLE_ENDIAN)
-#define HOST_IP_PRINTQUAD_LITTLE_ENDIAN(ip)     		(uint8_t)(((ip)>>24)&0xff),(uint8_t)(((ip)>>16)&0xff),(uint8_t)(((ip)>>8)&0xff),(uint8_t)((ip)&0xff)
-
-
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-/* The host byte order is the same as network byte order, so these functions are all just identity.  */
-#  define NIPQUAD(ip)     		NETWORK_IP_PRINTQUAD_LITTLE_ENDIAN(ip)
-#else
-# if __BYTE_ORDER == __BIG_ENDIAN
-#  define NIPQUAD(ip)     		HOST_IP_PRINTQUAD_LITTLE_ENDIAN(ip)
-# endif
-#endif
-
-//extern bool 		g_b_exit;
-//extern struct 		sigaction g_sigact;
-//extern uint8_t* 	g_fd_mask;
-//extern uint32_t 	g_fd_map_size;
-
 
 const char* to_str_socket_type(int type)
 {
@@ -160,7 +70,6 @@ const char* to_str_socket_type_netstat_like(int type)
 	}
 	return "???";
 }
-
 
 // Print statistics for offloaded sockets
 void print_full_stats(socket_stats_t* p_si_stats, mc_grp_info_t* p_mc_grp_info, FILE* filename)
@@ -278,40 +187,6 @@ void print_full_stats(socket_stats_t* p_si_stats, mc_grp_info_t* p_mc_grp_info, 
 	}
 }
 
-// this enum is copied from tcp.c in lwip project
-enum tcp_state {
-	CLOSED      = 0,
-	LISTEN      = 1,
-	SYN_SENT    = 2,
-	SYN_RCVD    = 3,
-	ESTABLISHED = 4,
-	FIN_WAIT_1  = 5,
-	FIN_WAIT_2  = 6,
-	CLOSE_WAIT  = 7,
-	CLOSING     = 8,
-	LAST_ACK    = 9,
-	TIME_WAIT   = 10
-};
-
-// for lwip's tcp_state only
-static const char *state2str(tcp_state state)
-{
-	switch(state){
-	case CLOSED:	return "CLOSED";
-	case LISTEN:	return "LISTEN";
-	case SYN_SENT:	return "SYN_SENT";
-	case SYN_RCVD:	return "SYN_RCVD";
-	case ESTABLISHED:return "ESTABLISHED";
-	case FIN_WAIT_1:return "FIN_WAIT_1";
-	case FIN_WAIT_2:return "FIN_WAIT_2";
-	case CLOSE_WAIT:return "CLOSE_WAIT";
-	case CLOSING:	return "CLOSING";
-	case LAST_ACK:	return "LAST_ACK";
-	case TIME_WAIT:	return "TIME_WAIT";
-	default:		return "UNKNOWN";
-    }
-}
-
 // Print statistics headers for all sockets - used in case view mode is e_netstat_like
 void print_netstat_like_headers(FILE* file)
 {
@@ -356,7 +231,7 @@ void print_netstat_like(socket_stats_t* p_si_stats, mc_grp_info_t* , FILE* file,
 
 	const char * tcp_state = "";
 	if (p_si_stats->socket_type == SOCK_STREAM) {
-		tcp_state = state2str((enum tcp_state)p_si_stats->tcp_state);
+		tcp_state = tcp_state_str[((enum tcp_state)p_si_stats->tcp_state)];
 	}
 
 	fprintf(file, "%-11s %-10lu %d/%s\n",
