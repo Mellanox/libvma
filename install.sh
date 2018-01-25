ebed1fe75c48439fbba4f20a62aa5c281f2af130
Liran Oz <liranoz12@gmail.com>
2018-01-25 14:54:02 +0200

    issue: 1277085 Update recommended installation command
    The recommended installation command which mentioned in README.txt
    and install.sh should install all files the same location as MLNX_OFED.
    
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ VMA_VERSION="$VMA_LIBRARY_MAJOR.$VMA_LIBRARY_MINOR.$VMA_LIBRARY_REVISION-$VMA_LI
 #configure without parameters - good if you don't need to install. libvma.so will be in ./src/vma/.libs/libvma.so.
 #./configure
 #configure with parameters required for install
-#example: ./configure --with-ofed=/usr --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include/mellanox --docdir=/usr/share/doc/libvma-6.4.7-0 --sysconfdir=/etc
-./configure --with-ofed=/usr --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include/mellanox --docdir="/usr/share/doc/libvma-$VMA_VERSION" --sysconfdir=/etc
+#example: ./configure --with-ofed=/usr --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include --docdir=/usr/share/doc/libvma-8.5.2 --sysconfdir=/etc
+./configure --with-ofed=/usr --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include --docdir="/usr/share/doc/libvma-$VMA_VERSION" --sysconfdir=/etc
 make
 sudo make install
 
