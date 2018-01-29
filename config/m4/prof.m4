9c6094abf462d33b0f0f75643dbeff88269fa728
Liran Oz <lirano@mellanox.com>
2018-01-29 11:00:47 +0200

    issue: 1279599 Update License date to 2018
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- config/m4/prof.m4
+++ config/m4/prof.m4
@@ -1,6 +1,6 @@
 # prof.m4 - Profiling, instrumentation
 # 
-# Copyright (C) Mellanox Technologies Ltd. 2001-2017.  ALL RIGHTS RESERVED.
+# Copyright (C) Mellanox Technologies Ltd. 2001-2018.  ALL RIGHTS RESERVED.
 # See file LICENSE for terms.
 #
 
--- src/vma/lwip/cc_cubic.h
+++ src/vma/lwip/cc_cubic.h
@@ -49,7 +49,7 @@
  */
 
 /*
- * Copyright (c) 2001-2017 Mellanox Technologies, Ltd. All rights reserved.
+ * Copyright (c) 2001-2018 Mellanox Technologies, Ltd. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
