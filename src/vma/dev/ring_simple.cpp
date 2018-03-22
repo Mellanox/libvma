ded9476dc91dd991ca1fed7be87b38d1f5472d16
Igor Ivanov <igor.ivanov.va@gmail.com>
2018-03-22 18:26:55 +0200

    issue: 1284069 Add ib_ctx_handle::get_ibname()
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

--- src/vma/dev/ring_simple.cpp
+++ src/vma/dev/ring_simple.cpp
@@ -86,7 +86,7 @@ inline void ring_simple::send_status_handler(int ret, vma_ibv_send_wr* p_send_wq
 qp_mgr* ring_eth::create_qp_mgr(const ib_ctx_handler* ib_ctx, uint8_t port_num, struct ibv_comp_channel* p_rx_comp_event_channel)
 {
 #if defined(HAVE_INFINIBAND_MLX5_HW_H)
-	if (!m_b_is_hypervisor && qp_mgr::is_lib_mlx5(((ib_ctx_handler*)ib_ctx)->get_ibv_device()->name)) {
+	if (!m_b_is_hypervisor && qp_mgr::is_lib_mlx5(((ib_ctx_handler*)ib_ctx)->get_ibname())) {
 		return new qp_mgr_eth_mlx5(this, ib_ctx, port_num, p_rx_comp_event_channel, get_tx_num_wr(), get_partition());
 	}
 #endif
