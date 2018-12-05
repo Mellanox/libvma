8b0a3968e7ef56d030b673d1608771ce55136bec
Liran Oz <lirano@mellanox.com>
2018-12-05 15:17:34 +0000

    issue: 1575056 Fix deadlock during TCP connection establishment
    More
    
    Signed-off-by: Liran Oz <lirano@mellanox.com>

--- src/vma/sock/sockinfo_tcp.cpp
+++ src/vma/sock/sockinfo_tcp.cpp
@@ -3112,10 +3112,7 @@ bool sockinfo_tcp::is_readable(uint64_t *p_poll_sn, fd_array_t* p_fd_array)
 			if (m_n_rx_pkt_ready_list_count || ret <= 0) {
 				break;
 			}
-		}
-		else if (unlikely(m_rx_ring_map.empty())) {
-			break;
-		} else {
+		} else if (!m_rx_ring_map.empty()) {
 			rx_ring_map_t::iterator rx_ring_iter;
 			for (rx_ring_iter = m_rx_ring_map.begin(); rx_ring_iter != m_rx_ring_map.end(); rx_ring_iter++) {
 				if (rx_ring_iter->second->refcnt <= 0) {
@@ -3124,9 +3121,13 @@ bool sockinfo_tcp::is_readable(uint64_t *p_poll_sn, fd_array_t* p_fd_array)
 				ring* p_ring =  rx_ring_iter->first;
 				//g_p_lwip->do_timers();
 				ret = p_ring->poll_and_process_element_rx(p_poll_sn, p_fd_array);
-				if (m_n_rx_pkt_ready_list_count || ret <= 0)
+				if (m_n_rx_pkt_ready_list_count || ret <= 0) {
 					break;
+				}
 			}
+		} else {
+			// No available rx rings, break loop.
+			break;
 		}
 	}
 
