Mellanox/libvma

commit ee0a1b983a6f7840a7aef2eb9b03269dadd48c12

issue: 2233904 Check bonding device using netlink
Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

src/vma/util/utils.cpp
@@ -50,6 +50,9 @@
 #endif
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
+#ifdef HAVE_LIBNL3
+#include <netlink/route/link.h>
+#endif
 
 #include "utils/bullseye.h"
 #include "vlogger/vlogger.h"
@@ -109,7 +112,7 @@ int get_base_interface_name(const char *if_name, char *base_ifname, size_t sz_ba
 
 	//Am I already the base (not virtual, not alias, can be bond)
 	if ((!check_device_exist(if_name, VIRTUAL_DEVICE_FOLDER) ||
-		check_device_exist(if_name, BOND_DEVICE_FILE)) && !strstr(if_name, ":")) {
+		check_bond_device_exist(if_name)) && !strstr(if_name, ":")) {
 		snprintf(base_ifname, sz_base_ifname, "%s" ,if_name);
 		return 0;
 	}
@@ -138,7 +141,7 @@ int get_base_interface_name(const char *if_name, char *base_ifname, size_t sz_ba
 
 			if (check_device_exist(ifa->ifa_name, VIRTUAL_DEVICE_FOLDER)) {
 				//virtual
-				if (!check_device_exist(ifa->ifa_name, BOND_DEVICE_FILE)) {
+				if (!check_bond_device_exist(ifa->ifa_name)) {
 					continue;
 				}
 			}
@@ -793,6 +796,55 @@ size_t get_local_ll_addr(IN const char * ifname, OUT unsigned char* addr, IN int
 	return bytes_len; // success
 }
 
+bool check_bond_device_exist(const char* ifname)
+{
+#ifdef HAVE_LIBNL3
+	int ret = 0;
+	struct nl_cache *cache = NULL;
+	struct rtnl_link *link = NULL;
+	char *link_type = NULL;
+
+	struct nl_sock *nl_socket = nl_socket_alloc();
+	if (!nl_socket) {
+		goto out;
+	}
+	nl_socket_set_local_port(nl_socket, 0);
+	ret = nl_connect(nl_socket, NETLINK_ROUTE);
+	if (ret < 0) {
+		goto out;
+	}
+	ret = rtnl_link_alloc_cache(nl_socket, AF_UNSPEC, &cache);
+	if (!cache) {
+		goto out;
+	}
+	link = rtnl_link_get_by_name(cache, ifname);
+	if (!link) {
+		goto out;
+	}
+	link_type = rtnl_link_get_type(link);
+	if (link_type &&
+		(strcmp(link_type, "bond") != 0) &&
+		(strcmp(link_type, "team") != 0)) {
+		link_type = NULL;
+	}
+out:
+	if (link) {
+		rtnl_link_put(link);
+	}
+	if (cache) {
+		nl_cache_free(cache);
+	}
+	if (nl_socket) {
+		nl_socket_free(nl_socket);
+	}
+
+	return ((bool)link_type);
+#else
+	return check_device_exist(ifname, BOND_DEVICE_FILE);
+#endif
+}
+
+
 bool get_bond_name(IN const char* ifname, OUT char* bond_name, IN int sz)
 {
 	char upper_path[256];
@@ -938,13 +990,20 @@ bool get_bond_slaves_name_list(IN const char* bond_name, OUT char* slaves_list, 
 bool check_device_exist(const char* ifname, const char *path)
 {
 	char device_path[256] = {0};
-	sprintf(device_path, path, ifname);
-	int fd = orig_os_api.open(device_path, O_RDONLY);
-	if (fd >= 0)
-		orig_os_api.close(fd);
-	if (fd < 0 && errno == EMFILE) {
-		__log_warn("There are no free fds in the system. This may cause unexpected behavior");
+	int fd = -1;
+	int n = -1;
+
+	n = snprintf(device_path, sizeof(device_path), path, ifname);
+	if (likely((0 < n) && (n < (int)sizeof(device_path)))) {
+		fd = orig_os_api.open(device_path, O_RDONLY);
+		if (fd >= 0) {
+			orig_os_api.close(fd);
+		}
+		if (fd < 0 && errno == EMFILE) {
+			__log_warn("There are no free fds in the system. This may cause unexpected behavior");
+		}
 	}
+
 	return (fd > 0);
 }
 

src/vma/dev/ib_ctx_handler_collection.cpp
@@ -196,7 +196,7 @@ ib_ctx_handler* ib_ctx_handler_collection::get_ib_ctx(const char *ifa_name)
 			return NULL;
 		}
 		ifa_name = (const char *)active_slave;
-	} else if (check_device_exist(ifa_name, BOND_DEVICE_FILE)) {
+	} else if (check_bond_device_exist(ifa_name)) {
 		/* active/backup: return active slave */
 		if (!get_bond_active_slave_name(ifa_name, active_slave, sizeof(active_slave))) {
 			char slaves[IFNAMSIZ * 16] = {0};

src/vma/dev/net_device_val.cpp
@@ -250,7 +250,7 @@ net_device_val::net_device_val(struct net_device_val_desc *desc) : m_lock("net_d
 	}
 
 	/* Identify device type */
-	if ((get_flags() & IFF_MASTER) || check_device_exist(get_ifname_link(), BOND_DEVICE_FILE)) {
+	if ((get_flags() & IFF_MASTER) || check_bond_device_exist(get_ifname_link())) {
 		verify_bonding_mode();
 	} else if (check_netvsc_device_exist(get_ifname_link())) {
 		m_bond = NETVSC;

src/vma/util/utils.h
@@ -260,7 +260,7 @@ size_t get_local_ll_addr(const char* ifname, unsigned char* addr, int addr_len, 
 /* Print warning while RoCE Lag is enabled */
 void print_roce_lag_warnings(char* interface, char* disable_path = NULL, const char* port1 = NULL, const char* port2 = NULL);
 
-bool get_bond_master_name(IN const char* ifname, OUT char* bond_name, IN int sz);
+bool check_bond_device_exist(const char* ifname);
 bool get_bond_active_slave_name(IN const char* bond_name, OUT char* active_slave_name, IN int sz);
 bool get_bond_slave_state(IN const char* slave_name, OUT char* curr_state, IN int sz);
 bool get_bond_slaves_name_list(IN const char* bond_name, OUT char* slaves_list, IN int sz);

commit 541e601f1cdc56a2ee142ddc5e3f28ffa507ed65

issue: 2446197 Skip team interface when check bonding
Both team and bonding Master interface has IFF_MASTER flag bit set.

However, linux sys files, such as BONDING_MODE_PARAM_FILE,  are used
to check bonding interface. The linux 'team' kernel module does not
create those files as 'bonding' module.

In other words, 'bonding' interface verify does not work for 'team'
interface, so skip 'team' interface when check 'bonding'.

Otherwise, we will have significant performance issues for UDP traffic
over 'team' interface.

Fixes: ee0a1b983a6f ("issue: 2233904 Check bonding device using netlink")

Signed-off-by: Honggang Li <honli@redhat.com>
Reviewed-by: Igor Ivanov <igori@nvidia.com>

src/vma/util/utils.cpp
@@ -822,9 +822,7 @@ bool check_bond_device_exist(const char* ifname)
 		goto out;
 	}
 	link_type = rtnl_link_get_type(link);
-	if (link_type &&
-		(strcmp(link_type, "bond") != 0) &&
-		(strcmp(link_type, "team") != 0)) {
+	if (link_type && (strcmp(link_type, "bond") != 0)) {
 		link_type = NULL;
 	}
 out: