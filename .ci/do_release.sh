25f17b3d0f562528f5df3210eb008419be47cc2a
NirWolfer <nwolfer@nvidia.com>
2024-07-22 17:28:27 +0300

    issue: 4007026 - [CI] Release pipeline
    Unti now, release process is done manually by the dev team. This process
    involves creating a tag on a commit that needs to be released, building
    that tag and copying the files to NFS location
    
    The idea is to create a jenkins pipeline that will do the entire process
    with given inputs, This commit will commence phase 1 - doing the build
    and copying of packages created to NFS.
    THE CREATION OF A RELEASE TAG ON A COMMIT IS NOT PART OF THIS PR
    
    Add release_jjb and the corosponding matrix file for a new pipeline
    definition, the release is run on rhel8.6
    Add do_release.sh script to run the build and NFS copy to release the
    package
    Add Dockerfile for release container with all dependencies installed
    
    Issue: HPCINFRA-1889
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

---
+++ .ci/do_release.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -Exel
+
+echo "**********************************"
+echo "Starting do_release.sh script..."
+echo "**********************************"
+
+set -o pipefail
+
+print_help() {
+    set +xv  
+    echo -e "\n\n"
+    echo "--------------------------------------------------"
+    echo "Usage: release_folder=<release folder> release_tag=<release tag> [revision=<revision>] [do_release=<true|false>] $0"
+    echo "       Where release folder is a path to NFS folder to copy the package into"
+    echo "       Where release tag is a git tag to release (must be already tagged in the git repo)"
+    echo "       Where revision is a number postfix to add the to package indicating which version of the tag it is - OPTIONAL, default value 1"
+    echo "       Where do_release is a boolean value indicating if the script will copy the created package into the release folder location - OPTIONAL, default value false"
+    exit 1
+}
+
+if [ -z "${release_folder}" ]; then
+    echo "ERROR: 'release_folder' was not set."
+    print_help
+fi
+
+if [ ! -e "${release_folder}" ] || [ ! -d "${release_folder}" ]; then
+    echo "ERROR: [${release_folder}] directory doesn't exist."
+    print_help
+fi
+
+if [ -z "${release_tag}" ]; then
+    echo "ERROR: 'release_tag' was not set."
+    print_help
+fi
+
+if [ -z "${revision}" ]; then
+    echo "WARN: 'revision' was not set, defaulting to 1"
+    revision=1
+fi
+
+if [ -z "${do_release}" ]; then
+    echo "WARN: 'do_release' was not set, defaulting to false (package will not be release)"
+    do_release=false
+fi
+
+env PRJ_RELEASE="${revision}" contrib/build_pkg.sh -s
+
+MAJOR_VERSION=$(grep -e "define(\[prj_ver_major\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
+MINOR_VERSION=$(grep -e "define(\[prj_ver_minor\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
+REVISION_VERSION=$(grep -e "define(\[prj_ver_revision\]" configure.ac | awk '{ printf $2 };' | sed  's/)//g')
+configure_ac_version="${MAJOR_VERSION}.${MINOR_VERSION}.${REVISION_VERSION}"
+echo "FULL_VERSION from configure.ac: [${configure_ac_version}]"
+
+if [[ "${release_tag}" != "${configure_ac_version}" ]]; then
+    echo "ERROR: FULL_VERSION: ${configure_ac_version} from configure.ac doesn't match tag: ${release_tag} provided! Exit"
+    exit 1
+fi
+
+if [ "${do_release}" = true ] ; then
+    echo "do_release is set to true, will release package into ${release_folder}/libvma_v_${release_tag}-0/src"
+
+    cd pkg/packages || { echo "pkg folder is missing, exiting..."; exit 1; }
+    pkg_name=$(ls -1 libvma-"${release_tag}"-"${revision}".src.rpm)
+    DST_DIR=${release_folder}/libvma_v_${release_tag}-0/src
+
+    if [[ -e "${DST_DIR}/${pkg_name}" ]]; then 
+        echo "ERROR: [${DST_DIR}/${pkg_name}] file already exist. Exit"
+        exit 1
+    fi
+
+    sudo -E -u swx-jenkins mkdir -p "$DST_DIR"
+    sudo -E -u swx-jenkins cp -v "${pkg_name}" "$DST_DIR"
+
+    cd "${release_folder}"
+    sudo -E -u swx-jenkins ln -s "$DST_DIR/${pkg_name}" latest_release
+
+    echo "Release found at $DST_DIR"
+
+else
+     echo "do_release is set to false, skipping package release."
+fi
+
+set +x
+echo "**********************************"
+echo "Finished do_release.sh script..."
+echo "**********************************"
