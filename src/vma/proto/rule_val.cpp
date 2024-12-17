0389fdac1dd022e93cb332b7b527a576fc8b064f
Tomer Cabouly <tcabouly@nvidia.com>
2024-12-17 14:57:33 +0200

    issue: 4223310 VMA support for kernel 6.10
    Kernel 6.10 netlink has breaked VMA functionality.
    Transitioned to libnl - an abstraction that wraps netlink.
    
    Signed-off-by: Tomer Cabouly <tcabouly@nvidia.com>

--- src/vma/proto/rule_val.cpp
+++ src/vma/proto/rule_val.cpp
@@ -30,7 +30,6 @@
  * SOFTWARE.
  */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -38,76 +37,77 @@
 #include "rule_val.h"
 #include "rule_table_mgr.h"
 
-#define MODULE_NAME 		"rrv"
+#define MODULE_NAME "rrv"
 
-#define rr_val_loginfo		__log_info_info
-#define rr_val_logdbg		__log_info_dbg
-#define rr_val_logfunc		__log_info_func
+#define rr_val_loginfo __log_info_info
+#define rr_val_logdbg  __log_info_dbg
+#define rr_val_logfunc __log_info_func
 
-rule_val::rule_val(): cache_observer()
+rule_val::rule_val()
+    : cache_observer()
 {
-	m_protocol = 0;
-	m_scope = 0;
-	m_type = 0;
-	m_dst_addr = 0;
-	m_src_addr = 0;
-	memset(m_oif_name, 0, IFNAMSIZ * sizeof(char));
-	memset(m_iif_name, 0, IFNAMSIZ * sizeof(char));
-	m_priority = 0;
-	m_tos = 0;
-	m_table_id = 0;
-	m_is_valid = false;
-	memset(m_str, 0, BUFF_SIZE * sizeof(char));
-	
+    m_dst_addr = 0;
+    m_src_addr = 0;
+    memset(m_oif_name, 0, IFNAMSIZ * sizeof(char));
+    memset(m_iif_name, 0, IFNAMSIZ * sizeof(char));
+    m_priority = 0;
+    m_tos = 0;
+    m_table_id = 0;
+    m_is_valid = false;
+    memset(m_str, 0, BUFF_SIZE * sizeof(char));
 }
 
 //This function build string that represent a row in the rule table.
 void rule_val::set_str()
 {
-	char str_addr[INET_ADDRSTRLEN];
-	char str_x[100] = {0};
+    char str_addr[INET_ADDRSTRLEN];
+    char str_x[100] = {0};
 
-	sprintf(m_str, "Priority :%-10u", m_priority);
+    sprintf(m_str, "Priority :%-10u", m_priority);
 
-	if (m_src_addr != 0) {
-		inet_ntop(AF_INET, &m_src_addr_in_addr, str_addr, sizeof(str_addr));
-		sprintf(str_x, " from :%-10s", str_addr);
-	}
-	strcat(m_str, str_x);
+    if (m_src_addr != 0) {
+        inet_ntop(AF_INET, &m_src_addr_in_addr, str_addr, sizeof(str_addr));
+        sprintf(str_x, " from :%-10s", str_addr);
+    }
+    strcat(m_str, str_x);
 
-	str_x[0] = '\0';
-	if (m_dst_addr != 0) {
-		inet_ntop(AF_INET, &m_dst_addr_in_addr, str_addr, sizeof(str_addr));
-		sprintf(str_x, " to :%-12s", str_addr);
-	}
-	strcat(m_str, str_x);
+    str_x[0] = '\0';
+    if (m_dst_addr != 0) {
+        inet_ntop(AF_INET, &m_dst_addr_in_addr, str_addr, sizeof(str_addr));
+        sprintf(str_x, " to :%-12s", str_addr);
+    }
+    strcat(m_str, str_x);
 
-	str_x[0] = '\0';
-    	if (m_tos != 0)	
-       		sprintf(str_x, " tos :%-11u", m_tos);
-	strcat(m_str, str_x);
+    str_x[0] = '\0';
+    if (m_tos != 0) {
+        sprintf(str_x, " tos :%-11u", m_tos);
+    }
+    strcat(m_str, str_x);
 
-	str_x[0] = '\0';
-    	if (strcmp(m_iif_name, "") != 0)
-		sprintf(str_x, " iif :%-11s", m_iif_name);
-	strcat(m_str, str_x);
+    str_x[0] = '\0';
+    if (strcmp(m_iif_name, "") != 0) {
+        sprintf(str_x, " iif :%-11s", m_iif_name);
+    }
+    strcat(m_str, str_x);
 
-	str_x[0] = '\0';
-    	if (strcmp(m_oif_name, "") != 0)
-		sprintf(str_x, " oif :%-11s", m_oif_name);		
-	strcat(m_str, str_x);
+    str_x[0] = '\0';
+    if (strcmp(m_oif_name, "") != 0) {
+        sprintf(str_x, " oif :%-11s", m_oif_name);
+    }
+    strcat(m_str, str_x);
 
-	str_x[0] = '\0';
-   	if (m_table_id != RT_TABLE_MAIN)
-		sprintf(str_x, " lookup table :%-10u", m_table_id);
-	else
-		sprintf(str_x, " lookup table :%-10s", "main");
-	strcat(m_str, str_x);
+    str_x[0] = '\0';
+    if (m_table_id != RT_TABLE_MAIN) {
+        sprintf(str_x, " lookup table :%-10u", m_table_id);
+    } else {
+        sprintf(str_x, " lookup table :%-10s", "main");
+    }
+    strcat(m_str, str_x);
 }
 
 //This function prints a string that represent a row in the rule table as debug log.
 void rule_val::print_val()
 {
-	set_str();
-	rr_val_logdbg("%s", to_str());
+    set_str();
+    rr_val_logdbg("%s", to_str());
 }
--- src/vma/proto/rule_val.h
+++ src/vma/proto/rule_val.h
@@ -30,7 +30,6 @@
  * SOFTWARE.
  */
 
-
 #ifndef RULE_VAL_H
 #define RULE_VAL_H
 
@@ -44,61 +43,53 @@
 /*
 This class will contain information for given routing rule entry.
 */
-class rule_val : public cache_observer
-{
+class rule_val : public cache_observer {
 public:
-	rule_val();
-	virtual ~rule_val() {};
+    rule_val();
+    virtual ~rule_val() {};
 
-	inline void set_dst_addr(in_addr_t const &dst_addr) 	{ m_dst_addr = dst_addr; };
-	inline void set_src_addr(in_addr_t const &src_addr) 	{ m_src_addr = src_addr; };
-	inline void set_protocol(unsigned char protocol) 	{ m_protocol = protocol; };
-	inline void set_scope(unsigned char scope) 		{ m_scope = scope; };
-	inline void set_type(unsigned char type) 		{ m_type = type; };
-	inline void set_tos(unsigned char tos) 			{ m_tos = tos; };
-	inline void set_table_id(uint32_t table_id) 		{ m_table_id = table_id; };
-	inline void set_iif_name(char *iif_name) 		{ memcpy(m_iif_name, iif_name, IFNAMSIZ); };
-	inline void set_oif_name(char *oif_name) 		{ memcpy(m_oif_name, oif_name, IFNAMSIZ); };
-	inline void set_priority(uint32_t priority) 		{ m_priority = priority; };
+    inline void set_dst_addr(in_addr_t const &dst_addr) { m_dst_addr = dst_addr; };
+    inline void set_src_addr(in_addr_t const &src_addr) { m_src_addr = src_addr; };
+    inline void set_tos(unsigned char tos) { m_tos = tos; };
+    inline void set_table_id(uint32_t table_id) { m_table_id = table_id; };
+    inline void set_iif_name(char *iif_name) { memcpy(m_iif_name, iif_name, IFNAMSIZ); };
+    inline void set_oif_name(char *oif_name) { memcpy(m_oif_name, oif_name, IFNAMSIZ); };
+    inline void set_priority(uint32_t priority) { m_priority = priority; };
 
-	void 	set_str();
+    void set_str();
 
-	inline in_addr_t	get_dst_addr()	const		{ return m_dst_addr; };
-	inline in_addr_t	get_src_addr() const		{ return m_src_addr; };
-	inline unsigned char	get_tos() const		   	{ return m_tos; };
-	inline uint32_t 	get_table_id() const		{ return m_table_id; };
-	inline const char*  	get_iif_name() const		{ return m_iif_name; };
-	inline const char*  	get_oif_name() const		{ return m_oif_name; };
+    inline in_addr_t get_dst_addr() const { return m_dst_addr; };
+    inline in_addr_t get_src_addr() const { return m_src_addr; };
+    inline unsigned char get_tos() const { return m_tos; };
+    inline uint32_t get_table_id() const { return m_table_id; };
+    inline const char *get_iif_name() const { return m_iif_name; };
+    inline const char *get_oif_name() const { return m_oif_name; };
 
-	inline void set_state(bool state) 			{ m_is_valid = state; };
-	inline bool is_valid() const		 		{ return m_is_valid; };
+    inline void set_state(bool state) { m_is_valid = state; };
+    inline bool is_valid() const { return m_is_valid; };
 
-	void 	print_val();
-	char* 	to_str() { return m_str; };
+    void print_val();
+    char *to_str() { return m_str; };
 
 private:
+    unsigned char m_tos;
 
-	unsigned char	m_protocol;
-	unsigned char	m_scope;
-	unsigned char	m_type;
-	unsigned char	m_tos;
+    union {
+        in_addr_t m_dst_addr;
+        in_addr m_dst_addr_in_addr;
+    };
+    union {
+        in_addr_t m_src_addr;
+        in_addr m_src_addr_in_addr;
+    };
+    char m_iif_name[IFNAMSIZ];
+    char m_oif_name[IFNAMSIZ];
+    uint32_t m_priority;
+    uint32_t m_table_id;
 
-	union {
-		in_addr_t 	m_dst_addr;
-		in_addr 	m_dst_addr_in_addr;
-	};
-	union {
-		in_addr_t 	m_src_addr;
-		in_addr 	m_src_addr_in_addr;
-	};
-	char 		m_iif_name[IFNAMSIZ];
-	char 		m_oif_name[IFNAMSIZ];
-	uint32_t	m_priority;
-	uint32_t	m_table_id;
-	
-	bool 		m_is_valid;
-	
-	char 		m_str[BUFF_SIZE];
+    bool m_is_valid;
+
+    char m_str[BUFF_SIZE];
 };
 
 #endif /* RULE_VAL_H */
