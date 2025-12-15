057b3bde6252d4f001de837b356ff66186436aca
NirWolfer <nwolfer@nvidia.com>
2025-12-15 14:04:02 +0200

    [CI] issue: 4568257 Run Blackduck as root
    Currently the Blackduck_source script runs as swx-jenkins, which is
    needed to run the git clone, but the causes issues when running the script
    
    Remove run as from the blackduck container
    
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

--- .ci/blackduck_source.sh
+++ .ci/blackduck_source.sh
@@ -25,7 +25,7 @@ echo "    SRC_PATH: ${PROJECT_SRC_PATH}"
 
 # clone BlackDuck
 [[ -d /tmp/blackduck ]] && rm -rf /tmp/blackduck
-git clone -c core.sshCommand="ssh -i ~/.ssh/id_ed25519" -b master --single-branch --depth=1 ssh://git-nbu.nvidia.com:12023/DevOps/Tools/blackduck /tmp/blackduck
+sudo -u swx-jenkins git clone -c core.sshCommand="ssh -l svc-nbu-swx-media -i ~/.ssh/id_ed25519_svc-nbu-swx-media_gerrit" -b master --single-branch --depth=1 ssh://git-nbu.nvidia.com:12023/DevOps/Tools/blackduck /tmp/blackduck
 cd /tmp/blackduck
 
 # disable check errors
